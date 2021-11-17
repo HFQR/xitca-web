295c7db6e54fd8985ecc20108ee1817cccfd0443
fakeshadow <24548779@qq.com>
2021-11-18 02:33:33 +0800

    simplify websocket example

--- examples/multi-services.rs
+++ examples/multi-services.rs
@@ -5,14 +5,14 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 use std::{convert::Infallible, io};
 
-use h3_quinn::quinn::generic::ServerConfig;
-use h3_quinn::quinn::{crypto::rustls::TlsSession, CertificateChain, PrivateKey, ServerConfigBuilder};
-use http::Version;
+use h3_quinn::quinn::{
+    crypto::rustls::TlsSession, generic::ServerConfig, CertificateChain, PrivateKey, ServerConfigBuilder,
+};
 use openssl::ssl::{AlpnError, SslAcceptor, SslFiletype, SslMethod};
 use xitca_http::{
     bytes::Bytes,
     h1, h2, h3,
-    http::{header, Request, Response},
+    http::{header, Request, Response, Version},
     util::Logger,
     HttpServiceBuilder, ResponseBody,
 };
@@ -31,25 +31,23 @@ async fn main() -> io::Result<()> {
     // construct http3 quic server config
     let config = h3_config()?;
 
+    let h1_factory = || HttpServiceBuilder::h1(fn_service(handler_h1)).with_logger();
+    let h2_factory = move || HttpServiceBuilder::h2(fn_service(handler_h2)).openssl(acceptor.clone());
+    let h3_factory = || HttpServiceBuilder::h3(fn_service(handler_h3)).transform(Logger::new());
+
     // construct server
     xitca_server::Builder::new()
         // bind to a http/1 service.
-        .bind::<_, _, _, TcpStream>("http/1", "127.0.0.1:8080", move || {
-            HttpServiceBuilder::h1(fn_service(handler_h1)).with_logger()
-        })?
+        .bind::<_, _, _, TcpStream>("http/1", "127.0.0.1:8080", h1_factory)?
         // bind to a http/2 service.
         // *. http/1 and http/2 both use tcp listener so it should be using a separate port.
-        .bind::<_, _, _, TcpStream>("http/2", "127.0.0.1:8081", move || {
-            HttpServiceBuilder::h2(fn_service(handler_h2)).openssl(acceptor.clone())
-        })?
+        .bind::<_, _, _, TcpStream>("http/2", "127.0.0.1:8081", h2_factory)?
         // bind to a http/3 service.
         // *. note the service name must be unique.
         //
         // Bind to same service with different bind_xxx API is allowed for reusing one service
         // on multiple socket addresses and protocols.
-        .bind_h3("http/3", "127.0.0.1:8080", config, move || {
-            HttpServiceBuilder::h3(fn_service(handler_h3)).transform(Logger::default())
-        })?
+        .bind_h3("http/3", "127.0.0.1:8080", config, h3_factory)?
         .build()
         .await
 }
@@ -73,12 +71,12 @@ async fn handler_h2(_: Request<h2::RequestBody>) -> Result<Response<ResponseBody
 }
 
 async fn handler_h3(_: Request<h3::RequestBody>) -> Result<Response<ResponseBody>, Box<dyn std::error::Error>> {
-    let res = Response::builder()
+    Response::builder()
         .status(200)
         .version(Version::HTTP_3)
         .header("Content-Type", "text/plain; charset=utf-8")
-        .body(Bytes::from_static(b"Hello World from Http/3!").into())?;
-    Ok(res)
+        .body(Bytes::from_static(b"Hello World from Http/3!").into())
+        .map_err(Into::into)
 }
 
 fn h2_config() -> io::Result<SslAcceptor> {
@@ -110,9 +108,9 @@ fn h3_config() -> io::Result<ServerConfig<TlsSession>> {
     config.protocols(&[b"h3-29", b"h3-28", b"h3-27"]);
 
     let key = std::fs::read("./cert/key.pem")?;
-    let key = PrivateKey::from_pem(&key).unwrap();
-
     let cert = std::fs::read("./cert/cert.pem")?;
+
+    let key = PrivateKey::from_pem(&key).unwrap();
     let cert = CertificateChain::from_pem(&cert).unwrap();
 
     config.certificate(cert, key).unwrap();
