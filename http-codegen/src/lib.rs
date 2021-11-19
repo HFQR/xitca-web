7c06d34af9f4f5e7034d05072c38f0b646b1a721
fakeshadow <24548779@qq.com>
2021-11-19 10:52:27 +0800

    update codegen and test

--- http-codegen/src/lib.rs
+++ http-codegen/src/lib.rs
@@ -1,66 +1,92 @@
 use proc_macro::TokenStream;
-use quote::{quote};
-use syn::{Ident, ImplItem, ImplItemMethod, Type};
-
+use quote::quote;
+use syn::{FnArg, GenericArgument, ImplItem, ImplItemMethod, PathArguments, ReturnType, Type};
 
 #[proc_macro_attribute]
 pub fn service_impl(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(item as syn::ItemImpl);
 
     // Collect type path from impl.
-    let ty = match input.self_ty.as_ref() {
+    let service_ty = match input.self_ty.as_ref() {
         Type::Path(path) => path,
-        _ => panic!("service_impl macro must be used on a TypePath")
+        _ => panic!("service_impl macro must be used on a TypePath"),
     };
 
     // collect generics.
     let generics = &input.generics;
 
     // find methods from impl.
-    let new_service_impl = find_async_method(&input.items, "new_service").expect("new_service method can not be located");
+    let new_service_impl =
+        find_async_method(&input.items, "new_service").expect("new_service method can not be located");
 
     // collect ServiceFactory, Config and InitError type from new_service_impl
-    
-    // println!("{:#?}", new_service_impl.sig);
-    
+    let mut inputs = new_service_impl.sig.inputs.iter();
+    let factory_ty = match inputs.next().unwrap() {
+        FnArg::Receiver(_) => panic!("new_service method does not accept Self as receiver"),
+        FnArg::Typed(ty) => match ty.ty.as_ref() {
+            Type::Reference(ty_ref) if ty_ref.mutability.is_none() => &ty_ref.elem,
+            _ => panic!("new_service must receive ServiceFactory type as immutable reference"),
+        },
+    };
+    let config_ty = match inputs.next().unwrap() {
+        FnArg::Receiver(_) => panic!("new_service method must not receive Self as receiver"),
+        FnArg::Typed(ty) => &ty.ty,
+    };
+    let (_, init_err_ty) = extract_res_ty(&new_service_impl.sig.output);
 
-    let ready_impl = find_async_method(&input.items, "ready").expect("ready method can not be located");
+    // make sure async fn ready is there and move on.
+    // TODO: Check the first fn arg and make sure it's a Receiver of &Self.
+    let _ = find_async_method(&input.items, "ready").expect("ready method can not be located");
+
+    // collect Request, Response and Error type.
     let call_impl = find_async_method(&input.items, "call").expect("call method can not be located");
 
+    let mut inputs = call_impl.sig.inputs.iter();
+    // ignore receiver and move on.
+    // TODO: Check the first fn arg and make sure it's a Receiver of &Self.
+    let _ = inputs.next().unwrap();
+
+    let req_ty = match inputs.next().unwrap() {
+        FnArg::Receiver(_) => panic!("new_service method does not accept Self as receiver"),
+        FnArg::Typed(ty) => &ty.ty,
+    };
+
+    let (res_ty, err_ty) = extract_res_ty(&call_impl.sig.output);
+
     let result = quote! {
-        impl ::xitca_service::ServiceFactory<()> for TestFactory {
-            type Response = ();
-            type Error = ();
-            type Config = ();
-            type Service = #ty;
-            type InitError = ();
+        impl ::xitca_service::ServiceFactory<#req_ty> for #factory_ty {
+            type Response = #res_ty;
+            type Error = #err_ty;
+            type Config = #config_ty;
+            type Service = #service_ty;
+            type InitError = #init_err_ty;
             type Future = impl ::core::future::Future<Output = Result<Self::Service, Self::InitError>>;
 
             fn new_service(&self, cfg: Self::Config) -> Self::Future {
                 let this = self.clone();
                 async move {
-                    #ty::new_service(&this, cfg).await
+                    #service_ty::new_service(&this, cfg).await
                 }
             }
         }
 
-        impl<#generics> ::xitca_service::Service<()> for #ty {
-            type Response = ();
-            type Error = ();
+        impl<#generics> ::xitca_service::Service<#req_ty> for #service_ty {
+            type Response = #res_ty;
+            type Error = #err_ty;
             type Ready<'f> where Self: 'f = impl ::core::future::Future<Output = Result<(), Self::Error>>;
             type Future<'f> where Self: 'f = impl ::core::future::Future<Output = Result<Self::Response, Self::Error>>;
 
             #[inline]
             fn ready(&self) -> Self::Ready<'_> {
                 async move {
-                    #ty::ready(self).await
+                    #service_ty::ready(self).await
                 }
             }
 
             #[inline]
-            fn call(&self, req: ()) -> Self::Future<'_> {
+            fn call(&self, req: #req_ty) -> Self::Future<'_> {
                 async move {
-                    #ty::call(self, req).await
+                    #service_ty::call(self, req).await
                 }
             }
         }
@@ -71,16 +97,32 @@ pub fn service_impl(_attr: TokenStream, item: TokenStream) -> TokenStream {
     result.into()
 }
 
-
 fn find_async_method<'a>(items: &'a [ImplItem], ident_str: &'a str) -> Option<&'a ImplItemMethod> {
-    items.iter()
-        .find_map(|item| match item {
-        ImplItem::Method(method) 
-        if method.sig.ident.to_string().as_str() == ident_str 
-         =>{
-             assert!(method.sig.asyncness.is_some(), "{} method must be async fn", ident_str);
+    items.iter().find_map(|item| match item {
+        ImplItem::Method(method) if method.sig.ident.to_string().as_str() == ident_str => {
+            assert!(method.sig.asyncness.is_some(), "{} method must be async fn", ident_str);
             Some(method)
-         },
+        }
         _ => None,
     })
+}
+
+// Extract Result<T, E> types from a return type of function.
+fn extract_res_ty(ret: &ReturnType) -> (&Type, &Type) {
+    if let ReturnType::Type(_, ty) = ret {
+        if let Type::Path(path) = ty.as_ref() {
+            let seg = path.path.segments.first().unwrap();
+            if seg.ident.to_string().as_str() == "Result" {
+                if let PathArguments::AngleBracketed(ref arg) = seg.arguments {
+                    if let (Some(GenericArgument::Type(ok_ty)), Some(GenericArgument::Type(err_ty))) =
+                        (arg.args.first(), arg.args.last())
+                    {
+                        return (ok_ty, err_ty);
+                    }
+                }
+            }
+        }
+    }
+
+    panic!("new_service method must output Result<Self, <InitError>>")
 }
--- test/tests/macro.rs
+++ test/tests/macro.rs
@@ -1,9 +1,8 @@
 #![feature(generic_associated_types, type_alias_impl_trait)]
 
-use xitca_http::{
-    body::{RequestBody, ResponseBody},
-    http::{Request, Response},
-};
+use std::convert::Infallible;
+
+use xitca_service::{Service, ServiceFactory};
 
 struct Test;
 
@@ -12,15 +11,29 @@ struct TestFactory;
 
 #[xitca_http_codegen::service_impl]
 impl Test {
-    async fn new_service(factory: &TestFactory, _: ()) -> Result<Self, ()> {
+    async fn new_service(_: &TestFactory, cfg: String) -> Result<Self, Infallible> {
+        assert_eq!(cfg.as_str(), "996");
         Ok(Test)
     }
 
     async fn ready(&self) -> Result<(), Box<dyn std::error::Error>> {
-        Ok(())
+        Err("251".into())
     }
 
-    async fn call(&self, req: Request<RequestBody>) -> Result<Response<ResponseBody>, Box<dyn std::error::Error>> {
-        todo!()
+    async fn call(&self, req: String) -> Result<usize, Box<dyn std::error::Error>> {
+        assert_eq!(req.as_str(), "007");
+
+        Ok(233)
     }
+}
+
+#[tokio::test]
+async fn http_codegen() {
+    let factory = TestFactory;
+    let cfg = String::from("996");
+    let test = ServiceFactory::new_service(&factory, cfg).await.unwrap();
+
+    Service::ready(&test).await.err().unwrap();
+    let res = Service::call(&test, String::from("007")).await.unwrap();
+    assert_eq!(res, 233);
 }
