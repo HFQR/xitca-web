68f05fe6289a77ef3bc0fc5e4957c95b75b13bd0
fakeshadow <24548779@qq.com>
2021-11-28 22:34:22 +0800

    add concurrent extractor future.

--- http/src/util/service/handler.rs
+++ http/src/util/service/handler.rs
@@ -1,10 +1,13 @@
 #![allow(non_snake_case)]
 
-use std::{convert::Infallible, future::Future};
+use std::{
+    cell::RefCell,
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
 
 use xitca_service::{fn_service, ServiceFactory};
-
-use crate::http::Request;
 
 pub fn handler_service<F, Req, T>(
     func: F,
@@ -14,10 +17,11 @@ where
     F::Output: Future,
     T: FromRequest<Req>,
 {
-    fn_service(move |mut req| {
+    fn_service(move |req| {
         let func = func.clone();
+        let req = RefCell::new(req);
         async move {
-            let t = T::from_request(&mut req).await?;
+            let t = T::from_request(&req).await?;
             Ok(func.call(t).await)
         }
     })
@@ -29,11 +33,11 @@ pub trait FromRequest<Req>: Sized {
     where
         Req: 'f;
 
-    fn from_request(req: &mut Req) -> Self::Future<'_>;
+    fn from_request(req: &RefCell<Req>) -> Self::Future<'_>;
 }
 
 macro_rules! from_req_impl {
-    ($($req: ident),*) => {
+    ($fut: ident; $(($n: tt, $req: ident)),*) => {
         impl<Req, Err, $($req,)*> FromRequest<Req> for ($($req,)*)
         where
             $(
@@ -46,28 +50,69 @@ macro_rules! from_req_impl {
                 Req: 'f,
             = impl Future<Output = Result<Self, Self::Error>>;
 
-            fn from_request(req: &mut Req) -> Self::Future<'_> {
-                async move {
-                    Ok((
-                        $(
-                            $req::from_request(req).await?,
-                        )*
+            fn from_request(req: &RefCell<Req>) -> Self::Future<'_> {
+                $fut {
+                    items: <($(Option<$req>,)+)>::default(),
+                    $(
+                        $req: $req::from_request(req),
+                    )+
+                }
+            }
+        }
+
+        pin_project_lite::pin_project! {
+                struct $fut<'f, Req, Err, $($req: FromRequest<Req, Error = Err>),+>
+                where
+                    Req: 'f
+                {
+                items: ($(Option<$req>,)+),
+                $(
+                    #[pin]
+                    $req: $req::Future<'f>,
+                )+
+            }
+        }
+
+        impl<Req, Err, $($req: FromRequest<Req, Error = Err>),+> Future for $fut<'_, Req, Err, $($req),+>
+        {
+            type Output = Result<($($req,)+), Err>;
+
+            fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+                let mut this = self.project();
+
+                let mut ready = true;
+                $(
+                    if this.items.$n.is_none() {
+                        match this.$req.poll(cx)? {
+                            Poll::Ready(item) => {
+                                this.items.$n = Some(item);
+                            }
+                            Poll::Pending => ready = false,
+                        }
+                    }
+                )+
+
+                if ready {
+                    Poll::Ready(Ok(
+                        ($(this.items.$n.take().unwrap(),)+)
                     ))
+                } else {
+                    Poll::Pending
                 }
             }
         }
     };
 }
 
-from_req_impl! { A }
-from_req_impl! { A, B }
-from_req_impl! { A, B, C }
-from_req_impl! { A, B, C, D }
-from_req_impl! { A, B, C, D, E }
-from_req_impl! { A, B, C, D, E, F }
-from_req_impl! { A, B, C, D, E, F, G }
-from_req_impl! { A, B, C, D, E, F, G, H }
-from_req_impl! { A, B, C, D, E, F, G, H, I }
+from_req_impl! { Extract1; (0, A) }
+from_req_impl! { Extract2; (0, A), (1, B) }
+from_req_impl! { Extract3; (0, A), (1, B), (2, C) }
+from_req_impl! { Extract4; (0, A), (1, B), (2, C), (3, D) }
+from_req_impl! { Extract5; (0, A), (1, B), (2, C), (3, D), (4, E) }
+from_req_impl! { Extract6; (0, A), (1, B), (2, C), (3, D), (4, E), (5, F) }
+from_req_impl! { Extract7; (0, A), (1, B), (2, C), (3, D), (4, E), (5, F), (6, G) }
+from_req_impl! { Extract8; (0, A), (1, B), (2, C), (3, D), (4, E), (5, F), (6, G), (7, H) }
+from_req_impl! { Extract9; (0, A), (1, B), (2, C), (3, D), (4, E), (5, F), (6, G), (7, H), (8, I) }
 
 pub trait Handler<Arg>: Clone {
     type Output;
