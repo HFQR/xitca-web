7c5f9be00498e5378bbe5f9e7d7de379672d8716
fakeshadow <24548779@qq.com>
2021-11-30 10:21:17 +0800

    reduce cost of cloing ServiceObject

--- service/src/service/object.rs
+++ service/src/service/object.rs
@@ -1,9 +1,6 @@
-use core::{
-    future::Future,
-    ops::{Deref, DerefMut},
-};
+use core::future::Future;
 
-use alloc::boxed::Box;
+use alloc::{boxed::Box, rc::Rc};
 
 use crate::BoxFuture;
 
@@ -14,43 +11,11 @@ use super::Service;
 /// [Service] trait uses GAT which does not offer object safety.
 /// This helper type offers the safety at the cost of tighter trait bound.
 /// (Service type, input Request type and output future type must bound to 'static lifetime.)
-pub struct ServiceObject<Req, Res, Err>(Box<dyn _ServiceObject<Req, Res, Err, Future = BoxFuture<'static, Res, Err>>>);
-
-impl<Req, Res, Err> Deref for ServiceObject<Req, Res, Err> {
-    type Target = dyn _ServiceObject<Req, Res, Err, Future = BoxFuture<'static, Res, Err>>;
-
-    fn deref(&self) -> &Self::Target {
-        &*self.0
-    }
-}
-
-impl<Req, Res, Err> DerefMut for ServiceObject<Req, Res, Err> {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut *self.0
-    }
-}
-
-impl<Req, Res, Err> ServiceObject<Req, Res, Err> {
-    pub(crate) fn new<S>(service: S) -> Self
-    where
-        S: Service<Req, Response = Res, Error = Err> + Clone + 'static,
-        Req: 'static,
-    {
-        Self(Box::new(service))
-    }
-}
-
-impl<Req, Res, Err> Clone for ServiceObject<Req, Res, Err> {
-    fn clone(&self) -> Self {
-        self.0.clone_object()
-    }
-}
+pub type ServiceObject<Req, Res, Err> = Rc<dyn _ServiceObject<Req, Res, Err, Future = BoxFuture<'static, Res, Err>>>;
 
 #[doc(hidden)]
 pub trait _ServiceObject<Req, Res, Err> {
     type Future: Future<Output = Result<Res, Err>>;
-
-    fn clone_object(&self) -> ServiceObject<Req, Res, Err>;
 
     fn call(&self, req: Req) -> Self::Future;
 }
@@ -61,11 +26,6 @@ where
     Req: 'static,
 {
     type Future = BoxFuture<'static, S::Response, S::Error>;
-
-    #[inline]
-    fn clone_object(&self) -> ServiceObject<Req, S::Response, S::Error> {
-        ServiceObject::new(self.clone())
-    }
 
     #[inline]
     fn call(&self, req: Req) -> Self::Future {
