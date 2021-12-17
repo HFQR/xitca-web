e9142adc69b53251f7fece0071a7c84b0ab415b7
fakeshadow <24548779@qq.com>
2021-12-17 21:18:54 +0800

    start implement postgres driver.

---
+++ postgres/src/error.rs
@@ -0,0 +1,32 @@
+use std::{error, fmt, io};
+
+use tokio::sync::mpsc::error::SendError;
+
+#[derive(Debug)]
+pub enum Error {
+    ConnectionClosed,
+    Io(io::Error),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Self::ConnectionClosed => write!(f, "Connection is closed"),
+            Self::Io(ref e) => write!(f, "{}", e),
+        }
+    }
+}
+
+impl error::Error for Error {}
+
+impl From<io::Error> for Error {
+    fn from(e: io::Error) -> Self {
+        Self::Io(e)
+    }
+}
+
+impl<T> From<SendError<T>> for Error {
+    fn from(_: SendError<T>) -> Self {
+        Self::ConnectionClosed
+    }
+}
---
+++ postgres/src/futures.rs
@@ -0,0 +1,125 @@
+use std::{
+    fmt,
+    future::Future,
+    pin::Pin,
+    task::{Context, Poll},
+};
+
+use pin_project_lite::pin_project;
+use tokio::time::Sleep;
+
+pub(crate) async fn never<T>() -> T {
+    poll_fn(|_| Poll::Pending).await
+}
+
+pub(crate) fn poll_fn<T, F>(f: F) -> PollFn<F>
+where
+    F: FnMut(&mut Context<'_>) -> Poll<T>,
+{
+    PollFn { f }
+}
+
+pub(crate) struct PollFn<F> {
+    f: F,
+}
+
+impl<F> Unpin for PollFn<F> {}
+
+impl<F> fmt::Debug for PollFn<F> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("PollFn").finish()
+    }
+}
+
+impl<T, F> Future for PollFn<F>
+where
+    F: FnMut(&mut Context<'_>) -> Poll<T>,
+{
+    type Output = T;
+
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<T> {
+        (&mut self.f)(cx)
+    }
+}
+
+pub(crate) trait Select: Sized {
+    fn select<Fut>(self, other: Fut) -> SelectFuture<Self, Fut>;
+}
+
+impl<F> Select for F
+where
+    F: Future,
+{
+    #[inline]
+    fn select<Fut>(self, other: Fut) -> SelectFuture<Self, Fut> {
+        SelectFuture {
+            fut1: self,
+            fut2: other,
+        }
+    }
+}
+
+pin_project! {
+    pub(crate) struct SelectFuture<Fut1, Fut2> {
+        #[pin]
+        fut1: Fut1,
+        #[pin]
+        fut2: Fut2,
+    }
+}
+
+impl<Fut1, Fut2> Future for SelectFuture<Fut1, Fut2>
+where
+    Fut1: Future,
+    Fut2: Future,
+{
+    type Output = SelectOutput<Fut1::Output, Fut2::Output>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+
+        if let Poll::Ready(a) = this.fut1.poll(cx) {
+            return Poll::Ready(SelectOutput::A(a));
+        }
+
+        this.fut2.poll(cx).map(SelectOutput::B)
+    }
+}
+
+pub(crate) enum SelectOutput<A, B> {
+    A(A),
+    B(B),
+}
+
+pub(crate) trait Timeout: Sized {
+    fn timeout(self, timer: Pin<&mut Sleep>) -> TimeoutFuture<'_, Self>;
+}
+
+impl<F> Timeout for F
+where
+    F: Future,
+{
+    fn timeout(self, timer: Pin<&mut Sleep>) -> TimeoutFuture<'_, Self> {
+        TimeoutFuture { fut: self, timer }
+    }
+}
+
+pin_project! {
+    pub(crate) struct TimeoutFuture<'a, F> {
+        #[pin]
+        fut: F,
+        timer: Pin<&'a mut Sleep>
+    }
+}
+
+impl<F: Future> Future for TimeoutFuture<'_, F> {
+    type Output = Result<F::Output, ()>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+        match this.fut.poll(cx) {
+            Poll::Ready(res) => Poll::Ready(Ok(res)),
+            Poll::Pending => this.timer.as_mut().poll(cx).map(Err),
+        }
+    }
+}
