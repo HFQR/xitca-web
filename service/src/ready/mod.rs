c3cfabdcb00d4465af6d69673bdde3d3063c791b
fakeshadow <24548779@qq.com>
2022-02-18 07:57:54 +0800

    add doc for ReadyService.

--- service/src/ready/mod.rs
+++ service/src/ready/mod.rs
@@ -11,6 +11,71 @@ use alloc::{boxed::Box, rc::Rc, sync::Arc};
 
 use super::service::Service;
 
+/// Extend trait for [Service].
+///
+/// Can be used to cehck the ready state of a service before calling it.
+///
+/// # Examples:
+/// ```rust
+/// #![feature(generic_associated_types, type_alias_impl_trait)]
+/// # use std::{cell::Cell, rc::Rc, future::Future};
+/// # use xitca_service::{Service, ready::ReadyService};
+///
+/// // a service with conditional avalibility based on state of Permit.
+/// struct Foo(Permit);
+///
+/// // a permit reset the inner boolean to true on drop.
+/// #[derive(Clone)]
+/// struct Permit(Rc<Cell<bool>>);
+///
+/// impl Drop for Permit {
+///     fn drop(&mut self) {
+///         self.0.set(true);
+///     }
+/// }
+///
+/// impl Service<()> for Foo {
+///     type Response = ();
+///     type Error = ();
+///     type Future<'f> = impl Future<Output = Result<Self::Response, Self::Error>>;
+///
+///     fn call(&self, _req: ()) -> Self::Future<'_> {
+///         async { Ok(()) }
+///     }
+/// }
+///
+/// impl ReadyService<()> for Foo {
+///     type Ready = Permit;
+///     type ReadyFuture<'f> = impl Future<Output = Result<Self::Ready, Self::Error>>;
+///
+///     fn ready(&self) -> Self::ReadyFuture<'_> {
+///         async move {
+///             if self.0.0.get() {
+///                 // set permit to false and return with Ok<Permit>
+///                 self.0.0.set(false);
+///                 Ok(self.0.clone())
+///             } else {
+///                 // return error is to simply the example.
+///                 // In real world this branch should be an async waiting for Permit reset to true.
+///                 Err(())
+///             }                
+///         }
+///     }
+/// }
+///
+/// async fn workflow(service: &Foo) {
+///     let permit = service.ready().await.unwrap(); // check service ready state.
+///
+///     service.call(()).await.unwrap(); // run Service::call when permit is held in scope.
+///
+///     drop(permit); // drop permit after Service::call is finished.
+/// }
+///
+/// async fn throttle(service: &Foo) {
+///     let permit = service.ready().await.unwrap();
+///     assert!(service.ready().await.is_err());  // service is throttled because permit is still helded in scope.
+/// }
+/// ```
 pub trait ReadyService<Req>: Service<Req> {
     type Ready;
 
