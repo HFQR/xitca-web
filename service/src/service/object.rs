14eeb114a3217b6f3adec180237b0624a89af12d
fakeshadow <24548779@qq.com>
2022-03-09 00:32:29 +0800

    fix service object doc.

--- service/src/service/object.rs
+++ service/src/service/object.rs
@@ -7,8 +7,6 @@ use super::Service;
 /// Trait object for type impls [Service] trait.
 ///
 /// [Service] trait uses GAT which does not offer object safety.
-/// This helper type offers the safety at the cost of tighter trait bound.
-/// (Service type, input Request type and output future type must bound to 'static lifetime.)
 pub type ServiceObject<Req, Res, Err> = Rc<dyn _ServiceObject<Req, Res, Err>>;
 
 #[doc(hidden)]
