91be975951ab98c81d05239ab7c7567b58581830
fakeshadow <24548779@qq.com>
2022-04-26 20:18:10 +0800

    fix route example.

--- examples/route.rs
+++ examples/route.rs
@@ -6,7 +6,7 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 use xitca_http::{
     body::{RequestBody, ResponseBody},
     http::{const_header_value::TEXT_UTF8, header::CONTENT_TYPE, IntoResponse, Response},
-    util::service::{get, Router},
+    util::service::{route::get, Router},
     HttpServiceBuilder, Request,
 };
 use xitca_service::fn_service;
