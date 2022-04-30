bd735792c9e33e52df0885918b82c2d8248f0d70
fakeshadow <24548779@qq.com>
2022-04-30 16:33:03 +0800

    add query extractor.

---
+++ web/src/handler/types/query.rs
@@ -0,0 +1,50 @@
+use std::{convert::Infallible, future::Future};
+
+use serde::Deserialize;
+
+use crate::{handler::FromRequest, request::WebRequest};
+
+const DEFAULT_LIMIT: usize = 1024 * 1024;
+
+pub struct Query<T, const LIMIT: usize = DEFAULT_LIMIT>(pub T);
+
+impl<'a, 'r, S, T, const LIMIT: usize> FromRequest<'a, WebRequest<'r, S>> for Query<T, LIMIT>
+where
+    S: 'r,
+    T: for<'de> Deserialize<'de>,
+{
+    type Type<'b> = Query<T, LIMIT>;
+    type Error = Infallible;
+    type Future = impl Future<Output = Result<Self, Self::Error>> where WebRequest<'r, S>: 'a;
+
+    fn from_request(req: &'a WebRequest<'r, S>) -> Self::Future {
+        async move {
+            let value = serde_urlencoded::from_str::<T>(req.req().uri().query().unwrap_or_default()).unwrap();
+            Ok(Query(value))
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    use crate::http::Uri;
+
+    #[derive(Deserialize)]
+    struct Id<'a> {
+        id: &'a str,
+    }
+
+    #[tokio::test]
+    async fn query() {
+        let mut req = WebRequest::new_test(());
+        let mut req = req.as_web_req();
+
+        *req.req_mut().uri_mut() = Uri::from_static("/996/251/?id=dagongren");
+
+        let Query(id) = Query::<Id<'_>>::from_request(&req).await.unwrap();
+
+        assert_eq!(id.id, "dagongren");
+    }
+}
