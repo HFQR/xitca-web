3f1dd4fb8e16b944bb50e111b9e06ed759134279
fakeshadow <24548779@qq.com>
2022-05-03 03:48:38 +0800

    add codegen feature to xitca-web.

--- web/src/lib.rs
+++ web/src/lib.rs
@@ -9,6 +9,35 @@ pub mod handler;
 pub mod request;
 pub mod response;
 
+#[cfg(feature = "codegen")]
+pub mod codegen {
+    /// Derive macro for individual struct field extractable through [StateRef](crate::handler::state::StateRef)
+    ///
+    /// # Example:
+    /// ```rust
+    /// # use xitca_web::{codegen::State, handler::{handler_service, state::StateRef}, App};
+    ///
+    /// // use derive macro and attribute to mark the field that can be extracted.
+    /// #[derive(State, Clone)]
+    /// struct MyState {
+    ///     #[borrow]
+    ///     field: u128
+    /// }
+    ///
+    /// # async fn app() {
+    /// // construct App with MyState type.
+    /// App::with_current_thread_state(MyState { field: 996})
+    ///     .at("/", handler_service(index));
+    /// # }
+    ///
+    /// // extract u128 typed field from MyState.
+    /// async fn index(StateRef(num): StateRef<'_, u128>) {
+    ///     assert_eq!(*num, 996);
+    /// }
+    /// ```
+    pub use xitca_http_codegen::State;
+}
+
 pub mod route {
     pub use xitca_http::util::service::route::{connect, delete, get, head, options, patch, post, put, trace, Route};
 }
