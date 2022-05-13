5b3891c7ef9d99c57f5dd31f390cf3413824149d
fakeshadow <24548779@qq.com>
2022-05-14 05:34:08 +0800

    remove phantomdata type from Extension mw.

--- http/src/util/middleware/extension.rs
+++ http/src/util/middleware/extension.rs
@@ -1,51 +1,44 @@
 use std::{
-    borrow::BorrowMut,
     convert::Infallible,
     future::{ready, Future, Ready},
-    marker::PhantomData,
 };
 
 use xitca_service::{ready::ReadyService, BuildService, Service};
 
-use crate::http;
+use crate::{http, request::BorrowReqMut};
 
 #[derive(Clone)]
-pub struct Extension<ReqB, F: Clone = ()> {
+pub struct Extension<F: Clone = ()> {
     factory: F,
-    _req_body: PhantomData<ReqB>,
 }
 
-impl<ReqB> Extension<ReqB> {
-    pub fn new<S>(state: S) -> Extension<ReqB, impl Fn() -> Ready<Result<S, Infallible>> + Clone>
+impl Extension {
+    pub fn new<S>(state: S) -> Extension<impl Fn() -> Ready<Result<S, Infallible>> + Clone>
     where
         S: Send + Sync + Clone + 'static,
     {
         Extension {
             factory: move || ready(Ok(state.clone())),
-            _req_body: PhantomData,
         }
     }
 
-    pub fn factory<F, Fut, Res, Err>(factory: F) -> Extension<ReqB, F>
+    pub fn factory<F, Fut, Res, Err>(factory: F) -> Extension<F>
     where
         F: Fn() -> Fut + Clone,
         Fut: Future<Output = Result<Res, Err>>,
         Res: Send + Sync + Clone + 'static,
     {
-        Extension {
-            factory,
-            _req_body: PhantomData,
-        }
+        Extension { factory }
     }
 }
 
-impl<S, ReqB, F, Fut, Res, Err> BuildService<S> for Extension<ReqB, F>
+impl<S, F, Fut, Res, Err> BuildService<S> for Extension<F>
 where
     F: Fn() -> Fut + Clone,
     Fut: Future<Output = Result<Res, Err>>,
     Res: Send + Sync + Clone + 'static,
 {
-    type Service = ExtensionService<S, ReqB, Res>;
+    type Service = ExtensionService<S, Res>;
     type Error = Err;
     type Future = impl Future<Output = Result<Self::Service, Self::Error>>;
 
@@ -54,22 +47,17 @@ where
 
         async {
             let state = fut.await?;
-            Ok(ExtensionService {
-                service,
-                state,
-                _req_body: PhantomData,
-            })
+            Ok(ExtensionService { service, state })
         }
     }
 }
 
-pub struct ExtensionService<S, ReqB, St> {
+pub struct ExtensionService<S, St> {
     service: S,
     state: St,
-    _req_body: PhantomData<ReqB>,
 }
 
-impl<S, ReqB, St> Clone for ExtensionService<S, ReqB, St>
+impl<S, St> Clone for ExtensionService<S, St>
 where
     S: Clone,
     St: Clone,
@@ -78,37 +66,36 @@ where
         Self {
             service: self.service.clone(),
             state: self.state.clone(),
-            _req_body: PhantomData,
         }
     }
 }
 
-impl<S, Req, ReqB, St> Service<Req> for ExtensionService<S, ReqB, St>
+impl<S, Req, St> Service<Req> for ExtensionService<S, St>
 where
     S: Service<Req>,
-    Req: BorrowMut<http::Request<ReqB>>,
+    Req: BorrowReqMut<http::Extensions>,
     St: Send + Sync + Clone + 'static,
 {
     type Response = S::Response;
     type Error = S::Error;
-    type Future<'f> = S::Future<'f> where S: 'f, ReqB: 'f;
+    type Future<'f> = S::Future<'f> where S: 'f;
 
     #[inline]
     fn call(&self, mut req: Req) -> Self::Future<'_> {
-        req.borrow_mut().extensions_mut().insert(self.state.clone());
+        req.borrow_mut().insert(self.state.clone());
         self.service.call(req)
     }
 }
 
-impl<S, Req, ReqB, St> ReadyService<Req> for ExtensionService<S, ReqB, St>
+impl<S, Req, St> ReadyService<Req> for ExtensionService<S, St>
 where
     S: ReadyService<Req>,
-    Req: BorrowMut<http::Request<ReqB>>,
+    Req: BorrowReqMut<http::Extensions>,
     St: Send + Sync + Clone + 'static,
 {
     type Ready = S::Ready;
 
-    type ReadyFuture<'f> = S::ReadyFuture<'f> where S: 'f, ReqB: 'f;
+    type ReadyFuture<'f> = S::ReadyFuture<'f> where S: 'f;
 
     #[inline]
     fn ready(&self) -> Self::ReadyFuture<'_> {
