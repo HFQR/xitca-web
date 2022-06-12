0f6f233289095aa803de4efd684e18ce94dc2e1a
fakeshadow <24548779@qq.com>
2022-06-13 07:13:06 +0800

    add async array.

--- unsafe_collection/src/mpsc.rs
+++ unsafe_collection/src/mpsc.rs
@@ -1,21 +1,139 @@
+//! An async aware mpsc array.
+
+extern crate alloc;
+
 use core::{
     cell::UnsafeCell,
+    future::Future,
+    pin::Pin,
     slice,
     sync::atomic::{fence, AtomicUsize, Ordering},
+    task::{Context, Poll},
 };
 
+use alloc::sync::Arc;
 use cache_padded::CachePadded;
 
-use super::uninit::{self, UninitArray};
+use super::{
+    futures::poll_fn,
+    uninit::{self, UninitArray},
+    waker::AtomicWaker,
+};
 
-pub struct AtomicArray<T, const N: usize> {
+pub fn async_array<T, const N: usize>() -> (Sender<T, N>, Receiver<T, N>) {
+    let array = Arc::new(AsyncArray {
+        // TODO: this is wrong and sender waker need a collection of wakers.
+        sender_waker: AtomicWaker::new(),
+        receiver_waker: AtomicWaker::new(),
+        array: AtomicArray::new(),
+    });
+
+    (Sender { inner: array.clone() }, Receiver { inner: array })
+}
+
+pub struct Sender<T, const N: usize> {
+    inner: Arc<AsyncArray<T, N>>,
+}
+
+impl<T, const N: usize> Sender<T, N> {
+    pub async fn send(&self, value: T) {
+        struct SendFuture<'a, T, const N: usize> {
+            sender: &'a Sender<T, N>,
+            value: Option<T>,
+        }
+
+        impl<T, const N: usize> Unpin for SendFuture<'_, T, N> {}
+
+        impl<T, const N: usize> Future for SendFuture<'_, T, N> {
+            type Output = ();
+
+            fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+                let this = self.get_mut();
+
+                match this.value.take() {
+                    Some(value) => match this.sender.inner.array.push_back(value) {
+                        Ok(_) => {
+                            this.sender.inner.receiver_waker.wake();
+                            Poll::Ready(())
+                        }
+                        Err(value) => {
+                            this.value = Some(value);
+                            this.sender.inner.sender_waker.register(cx.waker());
+                            Poll::Pending
+                        }
+                    },
+                    None => panic!("SendFuture polled after finish"),
+                }
+            }
+        }
+
+        SendFuture {
+            sender: self,
+            value: Some(value),
+        }
+        .await
+    }
+}
+
+pub struct Receiver<T, const N: usize> {
+    inner: Arc<AsyncArray<T, N>>,
+}
+
+impl<T, const N: usize> Receiver<T, N> {
+    /// wait for items to be available.
+    pub fn wait(&mut self) -> impl Future<Output = ()> + '_ {
+        poll_fn(|cx| {
+            if self.with_slice(|a, b| a.is_empty() && b.is_empty()) {
+                self.inner.receiver_waker.register(cx.waker());
+                Poll::Pending
+            } else {
+                Poll::Ready(())
+            }
+        })
+    }
+
+    /// peek into the available items inside array.
+    ///
+    /// # Order:
+    /// The two slices are presented in FIFO order. The items in first slice are always the ones
+    /// added earlier into the array. The items in each slice follow the same ordering that early
+    /// data always have smaller index than late ones.
+    #[inline]
+    pub fn with_slice<F, O>(&mut self, func: F) -> O
+    where
+        F: FnOnce(&[T], &[T]) -> O,
+    {
+        self.inner.array.with_slice(func)
+    }
+
+    /// Advance the array by iterate the array and pop drop items when given closure returns `true`.
+    #[inline]
+    pub fn advance_until<F>(&mut self, func: F)
+    where
+        F: FnMut(&mut T) -> bool,
+    {
+        self.inner.array.advance_until(func);
+        self.inner.sender_waker.wake();
+    }
+}
+
+struct AsyncArray<T, const N: usize> {
+    sender_waker: AtomicWaker,
+    receiver_waker: AtomicWaker,
+    array: AtomicArray<T, N>,
+}
+
+struct AtomicArray<T, const N: usize> {
     inner: UnsafeCell<UninitArray<T, N>>,
     next: CachePadded<AtomicUsize>,
     len: CachePadded<AtomicUsize>,
 }
 
+unsafe impl<T, const N: usize> Send for AtomicArray<T, N> where T: Send {}
+unsafe impl<T, const N: usize> Sync for AtomicArray<T, N> where T: Sync {}
+
 impl<T, const N: usize> AtomicArray<T, N> {
-    pub const fn new() -> Self {
+    const fn new() -> Self {
         Self {
             inner: UnsafeCell::new(UninitArray::new()),
             next: CachePadded::new(AtomicUsize::new(0)),
@@ -23,7 +141,7 @@ impl<T, const N: usize> AtomicArray<T, N> {
         }
     }
 
-    pub fn push_back(&self, value: T) -> Result<(), T> {
+    fn push_back(&self, value: T) -> Result<(), T> {
         let mut len = self.len.load(Ordering::Relaxed);
 
         loop {
@@ -33,7 +151,7 @@ impl<T, const N: usize> AtomicArray<T, N> {
 
             match self
                 .len
-                .compare_exchange_weak(len, len + 1, Ordering::AcqRel, Ordering::Relaxed)
+                .compare_exchange_weak(len, len + 1, Ordering::Acquire, Ordering::Relaxed)
             {
                 Ok(_) => {
                     let t = self.next.fetch_add(1, Ordering::Relaxed);
@@ -55,7 +173,7 @@ impl<T, const N: usize> AtomicArray<T, N> {
         }
     }
 
-    pub fn with_slice<F, O>(&self, func: F) -> O
+    fn with_slice<F, O>(&self, func: F) -> O
     where
         F: FnOnce(&[T], &[T]) -> O,
     {
@@ -81,13 +199,13 @@ impl<T, const N: usize> AtomicArray<T, N> {
         }
     }
 
-    pub fn advance_by<F>(&self, mut func: F)
+    fn advance_until<F>(&self, mut func: F)
     where
         F: FnMut(&mut T) -> bool,
     {
         loop {
             let len = self.len.load(Ordering::Acquire);
-            let idx = self.next.load(Ordering::Acquire);
+            let idx = self.next.load(Ordering::Relaxed);
 
             let tail = idx % N;
 
@@ -101,6 +219,9 @@ impl<T, const N: usize> AtomicArray<T, N> {
 
                 if func(&mut value) {
                     self.len.fetch_sub(1, Ordering::Release);
+                    if len == 1 {
+                        return;
+                    }
                 } else {
                     self.get_inner_mut().write_unchecked(head, value);
                     fence(Ordering::Release);
@@ -135,6 +256,12 @@ impl<T, const N: usize> AtomicArray<T, N> {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    use core::task::Waker;
+
+    use alloc::task::Wake;
+
+    use crate::pin;
 
     #[test]
     fn push() {
@@ -177,7 +304,7 @@ mod test {
             assert_eq!(b, &[]);
         });
 
-        array.advance_by(|i| *i != 3);
+        array.advance_until(|i| *i != 3);
 
         array.with_slice(|a, b| {
             assert_eq!(a, &[3]);
@@ -190,5 +317,88 @@ mod test {
             assert_eq!(a, &[3]);
             assert_eq!(b, &[4]);
         });
+    }
+
+    struct DummyWaker;
+
+    impl Wake for DummyWaker {
+        fn wake(self: Arc<Self>) {
+            // do nothing.
+        }
+    }
+
+    #[test]
+    fn spsc() {
+        let (tx, mut rx) = async_array::<usize, 8>();
+
+        let waker = Waker::from(Arc::new(DummyWaker));
+
+        let cx = &mut Context::from_waker(&waker);
+
+        for i in 0..8 {
+            let fut = tx.send(i);
+            pin!(fut);
+            assert!(fut.poll(cx).is_ready());
+        }
+
+        {
+            let fut = rx.wait();
+            pin!(fut);
+            assert!(fut.poll(cx).is_ready());
+        }
+
+        rx.with_slice(|a, b| {
+            assert_eq!(a.len(), 8);
+            assert_eq!(b.len(), 0);
+            assert_eq!(a[3], 3);
+        });
+
+        rx.advance_until(|i| *i < 8);
+
+        let fut = rx.wait();
+        pin!(fut);
+        assert!(fut.poll(cx).is_pending());
+    }
+
+    #[test]
+    #[cfg_attr(miri, ignore)]
+    fn race() {
+        let (tx, mut rx) = async_array::<u32, 8>();
+
+        let (tx1, rx1) = tokio::sync::oneshot::channel::<()>();
+
+        let h1 = std::thread::spawn(move || {
+            tokio::runtime::Builder::new_current_thread()
+                .build()
+                .unwrap()
+                .block_on(async {
+                    for i in 0..1024 {
+                        tx.send(i).await;
+                    }
+                    rx1.await.unwrap();
+                })
+        });
+
+        let h2 = std::thread::spawn(move || {
+            tokio::runtime::Builder::new_current_thread()
+                .build()
+                .unwrap()
+                .block_on(async {
+                    let mut outcome = 0;
+                    while outcome < 1024 - 1 {
+                        rx.wait().await;
+                        rx.with_slice(|a, b| {
+                            for i in a.iter().chain(b.iter()) {
+                                outcome = core::cmp::max(outcome, *i);
+                            }
+                        });
+                        rx.advance_until(|i| *i <= outcome);
+                    }
+                    tx1.send(()).unwrap();
+                })
+        });
+
+        h1.join().unwrap();
+        h2.join().unwrap();
     }
 }
---
+++ unsafe_collection/src/waker.rs
@@ -0,0 +1,99 @@
+//! atomic waker. copy/paste from futures-core.
+
+use core::{
+    cell::UnsafeCell,
+    fmt,
+    sync::atomic::{AtomicUsize, Ordering},
+    task::Waker,
+};
+
+pub(super) struct AtomicWaker {
+    state: AtomicUsize,
+    waker: UnsafeCell<Option<Waker>>,
+}
+
+const WAITING: usize = 0;
+
+const REGISTERING: usize = 0b01;
+
+const WAKING: usize = 0b10;
+
+impl AtomicWaker {
+    pub(super) const fn new() -> Self {
+        trait AssertSync: Sync {}
+        impl AssertSync for Waker {}
+
+        Self {
+            state: AtomicUsize::new(WAITING),
+            waker: UnsafeCell::new(None),
+        }
+    }
+
+    pub(super) fn register(&self, waker: &Waker) {
+        match self
+            .state
+            .compare_exchange(WAITING, REGISTERING, Ordering::Acquire, Ordering::Acquire)
+            .unwrap_or_else(|x| x)
+        {
+            WAITING => unsafe {
+                *self.waker.get() = Some(waker.clone());
+
+                let res = self
+                    .state
+                    .compare_exchange(REGISTERING, WAITING, Ordering::AcqRel, Ordering::Acquire);
+
+                match res {
+                    Ok(_) => {}
+                    Err(actual) => {
+                        debug_assert_eq!(actual, REGISTERING | WAKING);
+                        let waker = (*self.waker.get()).take().unwrap();
+                        self.state.swap(WAITING, Ordering::AcqRel);
+                        waker.wake();
+                    }
+                }
+            },
+            WAKING => waker.wake_by_ref(),
+            state => {
+                debug_assert!(state == REGISTERING || state == REGISTERING | WAKING);
+            }
+        }
+    }
+
+    pub(super) fn wake(&self) {
+        if let Some(waker) = self.take() {
+            waker.wake();
+        }
+    }
+
+    fn take(&self) -> Option<Waker> {
+        // AcqRel ordering is used in order to acquire the value of the `task`
+        // cell as well as to establish a `release` ordering with whatever
+        // memory the `AtomicWaker` is associated with.
+        match self.state.fetch_or(WAKING, Ordering::AcqRel) {
+            WAITING => {
+                let waker = unsafe { (*self.waker.get()).take() };
+                self.state.fetch_and(!WAKING, Ordering::Release);
+                waker
+            }
+            state => {
+                debug_assert!(state == REGISTERING || state == REGISTERING | WAKING || state == WAKING);
+                None
+            }
+        }
+    }
+}
+
+impl Default for AtomicWaker {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl fmt::Debug for AtomicWaker {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "AtomicWaker")
+    }
+}
+
+unsafe impl Send for AtomicWaker {}
+unsafe impl Sync for AtomicWaker {}
