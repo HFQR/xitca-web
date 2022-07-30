871ca380f0b94ad1517f02c334086bef4dd2fb22
fakeshadow <24548779@qq.com>
2022-07-31 02:19:56 +0800

    add experimental tower-http compatibility. (#299)

--- web/src/middleware/mod.rs
+++ web/src/middleware/mod.rs
@@ -2,3 +2,6 @@
 pub mod compress;
 #[cfg(any(feature = "compress-br", feature = "compress-gz", feature = "compress-de"))]
 pub mod decompress;
+
+#[cfg(feature = "tower-http-compat")]
+pub mod tower_http_compat;
