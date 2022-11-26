74ebd791052f62a1f9aa2989949840463c1b1800
fakeshadow <24548779@qq.com>
2022-11-27 06:51:21 +0800

    simplify signal handling. unify future types. (#395)
    * simplify signal handling. unify future types.
    
    * use boxed slice for multiple signals.
    
    Co-authored-by: “fakeshadow” <“24548779@qq.com”>

--- server/src/signals.rs
+++ server/src/signals.rs
@@ -1,8 +1,4 @@
-use std::{
-    future::Future,
-    pin::Pin,
-    task::{Context, Poll},
-};
+use std::{future::Future, pin::Pin};
 
 /// Different types of process signals
 #[allow(dead_code)]
@@ -18,69 +14,51 @@ pub(crate) enum Signal {
     Quit,
 }
 
-pub(crate) struct Signals {
-    #[cfg(unix)]
-    signals: Vec<(Signal, tokio::signal::unix::Signal)>,
-    #[cfg(not(unix))]
-    signals: Pin<Box<dyn Future<Output = std::io::Result<()>> + Send>>,
-}
+pub(crate) type SignalFuture = Pin<Box<dyn Future<Output = Signal> + Send>>;
 
-impl Signals {
+pub(crate) fn start() -> SignalFuture {
     #[cfg(unix)]
-    pub(crate) fn start() -> Self {
+    {
+        use std::{future::poll_fn, task::Poll};
+
         use tokio::signal::unix;
 
-        let sig_map = [
+        let mut signals = [
             (unix::SignalKind::interrupt(), Signal::Int),
             (unix::SignalKind::hangup(), Signal::Hup),
             (unix::SignalKind::terminate(), Signal::Term),
             (unix::SignalKind::quit(), Signal::Quit),
-        ];
+        ]
+        .iter()
+        .filter_map(|(kind, sig)| {
+            unix::signal(*kind)
+                .map(|tokio_sig| (*sig, tokio_sig))
+                .map_err(|e| tracing::error!("Can not initialize stream handler for {:?} err: {}", sig, e))
+                .ok()
+        })
+        .collect::<Vec<_>>()
+        .into_boxed_slice();
 
-        let signals = sig_map
-            .iter()
-            .filter_map(|(kind, sig)| {
-                unix::signal(*kind)
-                    .map(|tokio_sig| (*sig, tokio_sig))
-                    .map_err(|e| tracing::error!("Can not initialize stream handler for {:?} err: {}", sig, e))
-                    .ok()
-            })
-            .collect::<Vec<_>>();
-
-        Self { signals }
-    }
-
-    #[cfg(not(any(unix, target_family = "wasm")))]
-    pub(crate) fn start() -> Self {
-        Self {
-            signals: Box::pin(tokio::signal::ctrl_c()),
-        }
-    }
-
-    #[cfg(target_family = "wasm")]
-    pub(crate) fn start() -> Self {
-        Self {
-            signals: Box::pin(std::future::pending()),
-        }
-    }
-}
-
-impl Future for Signals {
-    type Output = Signal;
-
-    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        #[cfg(not(unix))]
-        {
-            self.signals.as_mut().poll(cx).map(|_| Signal::Int)
-        }
-        #[cfg(unix)]
-        {
-            for (sig, fut) in self.signals.iter_mut() {
+        Box::pin(poll_fn(move |cx| {
+            for (sig, fut) in signals.iter_mut() {
                 if Pin::new(fut).poll_recv(cx).is_ready() {
                     return Poll::Ready(*sig);
                 }
             }
             Poll::Pending
-        }
+        }))
+    }
+
+    #[cfg(not(any(unix, target_family = "wasm")))]
+    {
+        Box::pin(async {
+            let _ = tokio::signal::ctrl_c().await;
+            Signal::Int
+        })
+    }
+
+    #[cfg(target_family = "wasm")]
+    {
+        Box::pin(std::future::pending())
     }
 }
