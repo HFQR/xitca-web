fa5d90e2aac665063cb0da84df297940bd22e0a1
fakeshadow <24548779@qq.com>
2022-12-10 21:58:04 +0800

    remove Iterator impl from Params. (#427)

--- router/src/lib.rs
+++ router/src/lib.rs
@@ -1,12 +1,14 @@
 #![forbid(unsafe_code)]
 
+extern crate alloc;
+
 mod error;
 mod params;
 mod router;
 mod tree;
 
 pub use error::{InsertError, MatchError};
-pub use params::{Params, ParamsIter};
+pub use params::{Params, ParamsIntoIter};
 pub use router::{Match, Router};
 
 pub use xitca_unsafe_collection::bytes::BytesStr;
