e89726e4593737a2dd3246384a1b421a254d5e13
fakeshadow <24548779@qq.com>
2023-02-16 02:06:10 +0800

    rework postgres. prepare for adding transaction and pipeline support. (#494)

--- postgres/src/client.rs
+++ postgres/src/client.rs
@@ -37,7 +37,7 @@ impl Client {
     pub(crate) fn new(tx: UnboundedSender<Request>) -> Self {
         Self {
             tx,
-            buf: Lock::new(BytesMut::new()),
+            buf: Lock::new(BytesMut::with_capacity(4096)),
             cached_typeinfo: Lock::new(CachedTypeInfo {
                 typeinfo: None,
                 typeinfo_composite: None,
--- postgres/src/request.rs
+++ postgres/src/request.rs
@@ -4,13 +4,13 @@ use xitca_unsafe_collection::channel::spsc::channel;
 use super::{response::Response, response::ResponseSender};
 
 pub struct Request {
-    pub(crate) tx: Option<ResponseSender>,
+    pub(crate) tx: ResponseSender,
     pub(crate) msg: BytesMut,
 }
 
 impl Request {
     pub(crate) fn new_pair(msg: BytesMut) -> (Request, Response) {
         let (tx, rx) = channel(8);
-        (Request { tx: Some(tx), msg }, Response::new(rx))
+        (Request { tx, msg }, Response::new(rx))
     }
 }
