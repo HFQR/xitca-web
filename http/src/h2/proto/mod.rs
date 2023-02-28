4c2c647d1e3d6e7eb54c4bd29c4af6453eb6622b
fakeshadow <24548779@qq.com>
2023-02-28 15:32:57 +0800

    add task queue for multiplexing stream.

--- http/src/h2/proto/mod.rs
+++ http/src/h2/proto/mod.rs
@@ -10,17 +10,23 @@ mod stream_id;
 
 pub(crate) use dispatcher::Dispatcher;
 
-use std::{convert::Infallible, io};
+use core::{convert::Infallible, future::pending};
+
+use std::io;
 
 use xitca_io::{
     bytes::{Buf, BufMut, Bytes, BytesMut},
-    io::AsyncIo,
+    io::{AsyncIo, Interest, Ready},
 };
 use xitca_service::Service;
+use xitca_unsafe_collection::futures::{Select, SelectOutput};
 
 use crate::{
     http::{HeaderMap, Request, RequestExt, Response, StatusCode, Version},
-    util::buffered_io::{self, BufWrite, ListWriteBuf},
+    util::{
+        buffered_io::{self, BufWrite, ListWriteBuf},
+        futures::Queue,
+    },
 };
 
 use self::settings::Settings;
@@ -40,7 +46,7 @@ where
     let write_buf = ListWriteBuf::<Bytes, 32>::default();
     let mut io = BufferedIo::new(&mut io, write_buf);
 
-    prefix_check(&mut io).await?;
+    io.prefix_check().await?;
 
     let settings = Settings::default();
 
@@ -53,41 +59,62 @@ where
     let mut decoder = hpack::Decoder::new(settings::DEFAULT_SETTINGS_HEADER_TABLE_SIZE);
     let mut encoder = hpack::Encoder::new(65535, 4096);
 
+    let mut queue = Queue::new();
+
     loop {
-        let mut frame = recv_frame(&mut io).await?;
-        let head = head::Head::parse(&frame);
-        match head.kind() {
-            head::Kind::Settings => {
-                let _setting = Settings::load(head, &frame).unwrap();
+        match io.ready().select(queue.next()).await {
+            SelectOutput::A(ready) => {
+                let ready = ready?;
+
+                if ready.is_writable() {
+                    io.try_write()?;
+                }
+
+                if ready.is_readable() {
+                    io.try_read()?;
+
+                    while io.read_buf.len() >= 3 {
+                        let len = (io.read_buf.get_uint(3) + 6) as usize;
+                        if io.read_buf.len() < len {
+                            break;
+                        }
+
+                        let mut frame = io.read_buf.split_to(len);
+                        let head = head::Head::parse(&frame);
+                        match dbg!(head.kind()) {
+                            head::Kind::Settings => {
+                                // let _setting = Settings::load(head, &frame);
+                            }
+                            head::Kind::Headers => {
+                                // TODO: Make Head::parse auto advance the frame?
+                                frame.advance(6);
+
+                                let (mut headers, mut frame) = headers::Headers::load(head, frame).unwrap();
+
+                                headers.load_hpack(&mut frame, 4096, &mut decoder).unwrap();
+                                let (_pseudo, headers) = headers.into_parts();
+
+                                let mut req = Request::new(RequestExt::default());
+                                *req.version_mut() = Version::HTTP_2;
+                                *req.headers_mut() = headers;
+
+                                queue.push(service.call(req));
+                            }
+                            _ => {}
+                        }
+                    }
+                }
             }
-            head::Kind::Headers => {
-                // TODO: Make Head::parse auto advance the frame?
-                frame.advance(6);
-
-                let (mut headers, mut frame) = headers::Headers::load(head, frame).unwrap();
-
-                headers.load_hpack(&mut frame, 4096, &mut decoder).unwrap();
-                let (_pseudo, headers) = headers.into_parts();
-
-                let mut req = Request::new(RequestExt::default());
-                *req.version_mut() = Version::HTTP_2;
-                *req.headers_mut() = headers;
-
-                let (_parts, _body) = service.call(req).await.unwrap().into_parts();
-
+            SelectOutput::B(res) => {
+                let mut res = res.unwrap();
+                *res.version_mut() = Version::HTTP_2;
                 let pseudo = headers::Pseudo::response(StatusCode::OK);
-
                 let headers = headers::Headers::new(1.into(), pseudo, HeaderMap::new());
                 let mut buf = (&mut io.write_buf.buf).limit(4096);
                 headers.encode(&mut encoder, &mut buf);
-
                 let buf = io.write_buf.buf.split().freeze();
                 io.write_buf.buffer(buf);
-
                 break;
-            }
-            kind => {
-                dbg!(kind);
             }
         }
     }
@@ -96,42 +123,50 @@ where
     io.shutdown().await
 }
 
-#[cold]
-#[inline(never)]
-async fn prefix_check<Io, W>(io: &mut BufferedIo<'_, Io, W>) -> io::Result<()>
+impl<'a, Io, W> BufferedIo<'a, Io, W>
 where
     Io: AsyncIo,
     W: BufWrite,
 {
-    while io.read_buf.len() < PREFACE.len() {
-        io.read().await?;
+    #[cold]
+    #[inline(never)]
+    async fn prefix_check(&mut self) -> io::Result<()> {
+        while self.read_buf.len() < PREFACE.len() {
+            self.read().await?;
+        }
+
+        if &self.read_buf[..PREFACE.len()] == PREFACE {
+            self.read_buf.advance(PREFACE.len());
+        } else {
+            todo!()
+        }
+
+        Ok(())
     }
 
-    if &io.read_buf[..PREFACE.len()] == PREFACE {
-        io.read_buf.advance(PREFACE.len());
-    } else {
-        todo!()
+    async fn ready(&mut self) -> io::Result<Ready> {
+        let interest = match (self.read_buf.want_buf(), self.write_buf.want_write()) {
+            (true, true) => Interest::READABLE | Interest::WRITABLE,
+            (true, false) => Interest::READABLE,
+            (false, true) => Interest::WRITABLE,
+            _ => pending().await,
+        };
+        self.io.ready(interest).await
     }
 
-    Ok(())
-}
+    async fn recv_frame(&mut self) -> io::Result<BytesMut> {
+        while self.read_buf.len() < 3 {
+            self.read().await?;
+        }
 
-async fn recv_frame<Io, W>(io: &mut BufferedIo<'_, Io, W>) -> io::Result<BytesMut>
-where
-    Io: AsyncIo,
-    W: BufWrite,
-{
-    while io.read_buf.len() < 3 {
-        io.read().await?;
+        let len = (self.read_buf.get_uint(3) + 6) as usize;
+
+        while self.read_buf.len() < len {
+            self.read().await?;
+        }
+
+        Ok(self.read_buf.split_to(len))
     }
-
-    let len = (io.read_buf.get_uint(3) + 6) as usize;
-
-    while io.read_buf.len() < len {
-        io.read().await?;
-    }
-
-    Ok(io.read_buf.split_to(len))
 }
 
 /// A helper macro that unpacks a sequence of 4 bytes found in the buffer with
@@ -156,6 +191,7 @@ macro_rules! unpack_octets_4 {
     };
 }
 
+use crate::util::buffered_io::BufInterest;
 use unpack_octets_4;
 
 #[cfg(test)]
