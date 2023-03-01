0a93d95ba07fede0dd099876dde7ebdbc3c55ab3
fakeshadow <24548779@qq.com>
2023-03-01 14:01:02 +0800

    add hidden public api exposing postgres_types::FromSql trait.

--- postgres/src/row/types.rs
+++ postgres/src/row/types.rs
@@ -2,6 +2,7 @@ use core::{fmt, marker::PhantomData, ops::Range};
 
 use fallible_iterator::FallibleIterator;
 use postgres_protocol::message::backend::DataRowBody;
+use postgres_types::FromSql;
 use xitca_io::bytes::Bytes;
 
 use crate::{column::Column, error::Error, from_sql::FromSqlExt, Type};
@@ -73,6 +74,23 @@ impl<'a, C> GenericRow<'a, C> {
     fn col_buffer(&self, idx: usize) -> Option<(&Range<usize>, &Bytes)> {
         self.ranges[idx].as_ref().map(|r| (r, self.body.buffer_bytes()))
     }
+
+    fn get_idx_ty(
+        &self,
+        idx: impl RowIndexAndType + fmt::Display,
+        ty_check: impl FnOnce(&Type) -> bool,
+    ) -> Result<(usize, &Type), Error> {
+        let (idx, ty) = idx
+            ._from_columns(self.columns())
+            .ok_or(Error::InvalidColumnIndex(format!("{idx}")))?;
+
+        if !ty_check(ty) {
+            return Err(Error::ToDo);
+            // return Err(Error::from_sql(Box::new(WrongType::new::<T>(ty.clone())), idx));
+        }
+
+        Ok((idx, ty))
+    }
 }
 
 impl Row<'_> {
@@ -97,16 +115,32 @@ impl Row<'_> {
     where
         T: FromSqlExt<'s>,
     {
-        let (idx, ty) = idx
-            ._from_columns(self.columns())
-            .ok_or(Error::InvalidColumnIndex(format!("{idx}")))?;
-
-        if !T::accepts(ty) {
-            return Err(Error::ToDo);
-            // return Err(Error::from_sql(Box::new(WrongType::new::<T>(ty.clone())), idx));
-        }
-
+        let (idx, ty) = self.get_idx_ty(idx, T::accepts)?;
         FromSqlExt::from_sql_nullable_ext(ty, self.col_buffer(idx)).map_err(Into::into)
+    }
+
+    #[doc(hidden)]
+    /// hidden api for get row data with [FromSql] trait implementation.
+    pub fn get_raw<'s, T>(&'s self, idx: impl RowIndexAndType + fmt::Display) -> T
+    where
+        T: FromSql<'s>,
+    {
+        self.try_get_raw(&idx)
+            .unwrap_or_else(|e| panic!("error retrieving column {idx}: {e}"))
+    }
+
+    #[doc(hidden)]
+    /// hidden api for get row data with [FromSql] trait implementation.
+    pub fn try_get_raw<'s, T>(&'s self, idx: impl RowIndexAndType + fmt::Display) -> Result<T, Error>
+    where
+        T: FromSql<'s>,
+    {
+        let (idx, ty) = self.get_idx_ty(idx, T::accepts)?;
+        FromSql::from_sql_nullable(
+            ty,
+            self.ranges[idx].as_ref().map(|r| &self.body.buffer()[r.start..r.end]),
+        )
+        .map_err(Into::into)
     }
 }
 
@@ -123,11 +157,9 @@ impl RowSimple<'_> {
             .unwrap_or_else(|e| panic!("error retrieving column {idx}: {e}"))
     }
 
-    /// Like `SimpleQueryRow::get`, but returns a `Result` rather than panicking.
+    /// Like `RowSimple::get`, but returns a `Result` rather than panicking.
     pub fn try_get(&self, idx: impl RowIndexAndType + fmt::Display) -> Result<Option<&str>, Error> {
-        let (idx, _) = idx
-            ._from_columns(self.columns())
-            .ok_or(Error::InvalidColumnIndex(format!("{idx}")))?;
-        FromSqlExt::from_sql_nullable_ext(&Type::TEXT, self.col_buffer(idx)).map_err(Into::into)
+        let (idx, ty) = self.get_idx_ty(idx, <&str as FromSqlExt>::accepts)?;
+        FromSqlExt::from_sql_nullable_ext(ty, self.col_buffer(idx)).map_err(Into::into)
     }
 }
