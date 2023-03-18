e5735bd0c0ac4f8d26f42b30fae15da8e359384f
fakeshadow <24548779@qq.com>
2023-03-19 03:00:05 +0800

    add encode and sending logic.

--- postgres/src/transport/udp/codec.rs
+++ postgres/src/transport/udp/codec.rs
@@ -1,4 +1,13 @@
-use xitca_io::bytes::BytesMut;
+use xitca_io::bytes::{Bytes, BytesMut};
 
 #[derive(Debug)]
 pub(super) struct Codec;
+
+impl Codec {
+    pub(super) fn encode(&self, msg: BytesMut) -> [Bytes; 2] {
+        let msg = msg.freeze();
+        let len = (msg.len() as u64).to_be_bytes();
+        let prefix = Bytes::copy_from_slice(&len);
+        [prefix, msg]
+    }
+}
--- postgres/src/transport/udp/response.rs
+++ postgres/src/transport/udp/response.rs
@@ -1,10 +1,23 @@
 use postgres_protocol::message::backend;
 
+use quinn::RecvStream;
+use xitca_io::bytes::BytesMut;
+
 use crate::error::Error;
 
-pub struct Response;
+pub struct Response {
+    rx: RecvStream,
+    buf: BytesMut,
+}
 
 impl Response {
+    pub(super) fn new(rx: RecvStream) -> Self {
+        Self {
+            rx,
+            buf: BytesMut::new(),
+        }
+    }
+
     pub(crate) async fn recv(&mut self) -> Result<backend::Message, Error> {
         todo!()
     }
