26aec3b0926711d0855f83041df8f96061a23ebe
fakeshadow <24548779@qq.com>
2023-03-21 12:14:55 +0800

    add support for unix socket.

--- postgres/src/transport.rs
+++ postgres/src/transport.rs
@@ -7,3 +7,28 @@ pub use raw::*;
 mod quic;
 #[cfg(feature = "quic")]
 pub use quic::*;
+
+use xitca_service::AsyncClosure;
+
+use super::{
+    config::{Config, Host},
+    error::Error,
+};
+
+#[cold]
+#[inline(never)]
+pub(crate) async fn try_connect_multi<F, O>(cfg: &Config, func: F) -> Result<O, Error>
+where
+    F: for<'f> AsyncClosure<(&'f Host, &'f Config), Output = Result<O, Error>>,
+{
+    let mut err = None;
+
+    for host in cfg.get_hosts() {
+        match func.call((host, cfg)).await {
+            Ok(t) => return Ok(t),
+            Err(e) => err = Some(e),
+        }
+    }
+
+    Err(err.unwrap())
+}
