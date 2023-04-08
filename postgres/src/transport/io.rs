b4df6b725754a2b53ff7a7ae2697e5f8d0ce02d1
fakeshadow <24548779@qq.com>
2023-04-08 22:42:33 +0800

    add streaming of backend async message through buffered io.

--- postgres/src/transport/io.rs
+++ postgres/src/transport/io.rs
@@ -8,6 +8,7 @@ use alloc::collections::VecDeque;
 
 use std::io;
 
+use postgres_protocol::message::backend;
 use tokio::sync::mpsc::UnboundedReceiver;
 use tracing::error;
 use xitca_io::{
@@ -16,7 +17,7 @@ use xitca_io::{
 };
 use xitca_unsafe_collection::futures::{Select as _, SelectOutput};
 
-use crate::error::Error;
+use crate::{error::Error, iter::AsyncIterator};
 
 use super::codec::{Request, ResponseMessage, ResponseSender};
 
@@ -48,9 +49,12 @@ where
     Io: AsyncIo + Send + 'static,
     for<'f> Io::Future<'f>: Send,
 {
+    pub(crate) async fn run(mut self) -> Result<(), Error> {
+        self._run().await
+    }
+
     fn try_read(&mut self) -> Result<(), Error> {
-        self.read_buf.do_io(&mut self.io)?;
-        self.ctx.try_decode(&mut self.read_buf)
+        self.read_buf.do_io(&mut self.io).map_err(Into::into)
     }
 
     fn try_write(&mut self) -> io::Result<()> {
@@ -61,12 +65,17 @@ where
         })
     }
 
-    pub(crate) async fn run(mut self) -> Result<(), Error> {
-        self._run().await
+    async fn _run(&mut self) -> Result<(), Error> {
+        while self._next().await?.is_some() {}
+        Ok(())
     }
 
-    async fn _run(&mut self) -> Result<(), Error> {
+    async fn _next(&mut self) -> Result<Option<backend::Message>, Error> {
         loop {
+            if let Some(msg) = self.ctx.try_decode(&mut self.read_buf)? {
+                return Ok(Some(msg));
+            }
+
             let interest = if self.write_buf.want_write_io() {
                 Interest::READABLE | Interest::WRITABLE
             } else {
@@ -90,6 +99,10 @@ where
                         self.try_read()?;
                     }
                     if ready.is_writable() && self.try_write().is_err() {
+                        // write failed as server stopped reading.
+                        // close channel and drain drop all pending request.
+                        self.rx.close();
+                        while self.rx.try_recv().is_ok() {}
                         break;
                     }
                 }
@@ -102,9 +115,13 @@ where
 
     #[cold]
     #[inline(never)]
-    fn shutdown(&mut self) -> impl Future<Output = Result<(), Error>> + Send + '_ {
+    fn shutdown(&mut self) -> impl Future<Output = Result<Option<backend::Message>, Error>> + Send + '_ {
         Box::pin(async {
             loop {
+                if let Some(msg) = self.ctx.try_decode(&mut self.read_buf)? {
+                    return Ok(Some(msg));
+                }
+
                 let want_write = self.write_buf.want_write_io();
                 let want_read = !self.ctx.is_empty();
                 let interest = match (want_read, want_write) {
@@ -123,10 +140,24 @@ where
                 }
             }
 
-            poll_fn(|cx| Pin::new(&mut self.io).poll_shutdown(cx))
-                .await
-                .map_err(Into::into)
+            poll_fn(|cx| Pin::new(&mut self.io).poll_shutdown(cx)).await?;
+
+            Ok(None)
         })
+    }
+}
+
+impl<Io> AsyncIterator for BufferedIo<Io>
+where
+    Io: AsyncIo + Send + 'static,
+    for<'f> Io::Future<'f>: Send,
+{
+    type Future<'f> = impl Future<Output = Option<Self::Item<'f>>> + Send + 'f where Self: 'f;
+    type Item<'i> = Result<backend::Message, Error> where Self: 'i;
+
+    #[inline]
+    fn next(&mut self) -> Self::Future<'_> {
+        async { self._next().await.transpose() }
     }
 }
 
@@ -149,22 +180,25 @@ impl Context {
         self.concurrent_res.push_back(tx);
     }
 
-    fn try_decode(&mut self, buf: &mut PagedBytesMut) -> Result<(), Error> {
+    fn try_decode(&mut self, buf: &mut PagedBytesMut) -> Result<Option<backend::Message>, Error> {
         while let Some(res) = ResponseMessage::try_from_buf(buf)? {
-            if let ResponseMessage::Normal { buf, complete } = res {
-                let _ = self
-                    .concurrent_res
-                    .front_mut()
-                    .expect("Out of bound must not happen")
-                    .send(buf);
+            match res {
+                ResponseMessage::Normal { buf, complete } => {
+                    let _ = self
+                        .concurrent_res
+                        .front_mut()
+                        .expect("Out of bound must not happen")
+                        .send(buf);
 
-                if complete {
-                    let _ = self.concurrent_res.pop_front();
+                    if complete {
+                        let _ = self.concurrent_res.pop_front();
+                    }
                 }
+                ResponseMessage::Async(msg) => return Ok(Some(msg)),
             }
         }
 
-        Ok(())
+        Ok(None)
     }
 }
 
