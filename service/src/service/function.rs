228573a73c6e6f2e5d8aba0bc29b99f2d31ec36e
fakeshadow <24548779@qq.com>
2023-04-20 05:44:45 +0800

    remove duplicated FnBuild type.

--- service/src/service/function.rs
+++ service/src/service/function.rs
@@ -8,7 +8,16 @@ use core::{
 use super::Service;
 
 /// Shortcut for transform a given Fn into type impl [Service] trait.
-pub fn fn_service<F, Req, Fut, Res, Err>(f: F) -> FnBuild<impl Fn(()) -> Ready<Result<FnService<F>, Infallible>>>
+pub fn fn_build<F, Arg, Fut, Svc, Err>(f: F) -> FnService<F>
+where
+    F: Fn(Arg) -> Fut,
+    Fut: Future<Output = Result<Svc, Err>>,
+{
+    FnService(f)
+}
+
+/// Shortcut for transform a given Fn into type impl [Service] trait.
+pub fn fn_service<F, Req, Fut, Res, Err>(f: F) -> FnService<impl Fn(()) -> Ready<Result<FnService<F>, Infallible>>>
 where
     F: Fn(Req) -> Fut + Clone,
     Fut: Future<Output = Result<Res, Err>>,
@@ -16,41 +25,12 @@ where
     fn_build(move |_| ready(Ok(FnService(f.clone()))))
 }
 
-/// Shortcut for transform a given Fn into type impl [Service] trait.
-pub fn fn_build<F, Arg, Fut, Svc, Err>(f: F) -> FnBuild<F>
-where
-    F: Fn(Arg) -> Fut,
-    Fut: Future<Output = Result<Svc, Err>>,
-{
-    FnBuild(f)
-}
-
-#[derive(Clone)]
-pub struct FnBuild<F>(F);
-
-impl<F, Arg, Fut, Svc, Err> Service<Arg> for FnBuild<F>
-where
-    F: Fn(Arg) -> Fut,
-    Fut: Future<Output = Result<Svc, Err>>,
-{
-    type Response = Svc;
-    type Error = Err;
-    type Future<'f> = impl Future<Output = Result<Self::Response, Self::Error>> + 'f where Self: 'f, Arg: 'f;
-
-    fn call<'s>(&'s self, arg: Arg) -> Self::Future<'s>
-    where
-        Arg: 's,
-    {
-        async { (self.0)(arg).await }
-    }
-}
-
 #[derive(Clone)]
 pub struct FnService<F>(F);
 
 impl<F, Req, Fut, Res, Err> Service<Req> for FnService<F>
 where
-    F: Fn(Req) -> Fut + Clone,
+    F: Fn(Req) -> Fut,
     Fut: Future<Output = Result<Res, Err>>,
 {
     type Response = Res;
