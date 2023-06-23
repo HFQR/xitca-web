8e91c211fffedcc6c634bf33340a606c0dce6a10
fakeshadow <24548779@qq.com>
2023-06-23 12:21:41 +0800

    simplify task api to reduce misuse.

--- postgres/src/driver/io_uring.rs
+++ postgres/src/driver/io_uring.rs
@@ -1,6 +1,5 @@
 use core::{
     future::{pending, poll_fn, Future},
-    mem,
     task::Poll,
 };
 
@@ -25,45 +24,50 @@ use super::{
 
 type Opt = (io::Result<usize>, BytesMut);
 
-// a reusable boxed future.
-// it must poll one future to completion before the next one can be set.
-struct Task {
-    fut: ReusableLocalBoxFuture<'static, Opt>,
-    idle: bool,
+// completion based io task.
+// every task future take ownership of BytesMut and run to completion before the next task future
+// can be made.
+struct BufTask {
+    buf: Option<BytesMut>,
+    task: ReusableLocalBoxFuture<'static, Opt>,
 }
 
-impl Default for Task {
-    fn default() -> Self {
+impl BufTask {
+    fn new(buf: BytesMut) -> Self {
         Self {
-            fut: ReusableLocalBoxFuture::new(pending()),
-            idle: true,
+            buf: Some(buf),
+            task: ReusableLocalBoxFuture::new(pending()),
         }
     }
-}
 
-impl Task {
-    fn set(&mut self, fut: impl Future<Output = Opt> + 'static) {
-        debug_assert!(
-            mem::replace(&mut self.idle, false),
-            "Task must not be set multiple times before Task::poll is finished"
-        );
-        self.fut.set(fut);
+    fn set_with<F, Fut>(&mut self, func: F)
+    where
+        F: FnOnce(BytesMut) -> Fut,
+        Fut: Future<Output = Opt> + 'static,
+    {
+        let buf = self.buf.take().expect("BufferTask::poll must run to completion.");
+        self.task.set(func(buf));
     }
 
-    async fn poll(&mut self) -> Opt {
-        debug_assert!(!self.idle, "Task must not be polled before Task::set is called");
-        let res = self.fut.get_pin().await;
-        self.idle = true;
+    fn try_buf(&mut self) -> Option<&mut BytesMut> {
+        self.buf.as_mut()
+    }
+
+    async fn poll(&mut self) -> io::Result<usize> {
+        debug_assert!(
+            self.buf.is_none(),
+            "BufferTask must not be polled before BufferTask::set_with is called"
+        );
+        let (res, buf) = self.task.get_pin().await;
+        self.buf = Some(buf);
         res
     }
 }
 
 pub struct IoUringDriver<Io> {
     io: Rc<Io>,
-    read_task: Task,
-    write_task: Task,
-    write_buf: Option<BytesMut>,
-    read_buf: Option<BytesMut>,
+    read_task: BufTask,
+    write_task: BufTask,
     rx: GenericDriverRx,
     res: VecDeque<ResponseSender>,
 }
@@ -82,10 +86,8 @@ where
     ) -> Self {
         Self {
             io: Rc::new(io),
-            read_task: Task::default(),
-            write_task: Task::default(),
-            write_buf: Some(write_buf),
-            read_buf: Some(read_buf),
+            read_task: BufTask::new(read_buf),
+            write_task: BufTask::new(write_buf),
             rx,
             res,
         }
@@ -93,7 +95,7 @@ where
 
     pub(crate) async fn try_next(&mut self) -> Result<Option<backend::Message>, Error> {
         loop {
-            if let Some(ref mut buf) = self.read_buf {
+            if let Some(buf) = self.read_task.try_buf() {
                 while let Some(res) = ResponseMessage::try_from_buf(buf)? {
                     match res {
                         ResponseMessage::Normal { buf, complete } => {
@@ -106,23 +108,24 @@ where
                     }
                 }
 
-                let mut buf = self.read_buf.take().unwrap();
-                let len = buf.len();
-                let rem = buf.capacity() - len;
+                self.read_task.set_with(|mut buf| {
+                    let len = buf.len();
+                    let rem = buf.capacity() - len;
 
-                if rem < 4096 {
-                    buf.reserve(4096 - rem);
-                }
+                    if rem < 4096 {
+                        buf.reserve(4096 - rem);
+                    }
 
-                let io = self.io.clone();
-                self.read_task.set(async move {
-                    let (res, slice) = io.read(buf.slice(len..)).await;
-                    (res, slice.into_inner())
+                    let io = self.io.clone();
+                    async move {
+                        let (res, slice) = io.read(buf.slice(len..)).await;
+                        (res, slice.into_inner())
+                    }
                 });
             }
 
             let write_task = async {
-                while let Some(ref mut buf) = self.write_buf {
+                while let Some(buf) = self.write_task.try_buf() {
                     let res = poll_fn(|cx| match self.rx.poll_recv(cx) {
                         Poll::Ready(Some(req)) => Poll::Ready(SelectOutput::A(Some(req))),
                         Poll::Ready(None) if !buf.is_empty() => Poll::Ready(SelectOutput::B(())),
@@ -137,19 +140,20 @@ where
                             buf.extend_from_slice(req.msg.as_ref());
                             self.res.push_back(req.tx);
                         }
-                        SelectOutput::A(None) => return (Ok(0), self.write_buf.take().unwrap()),
+                        SelectOutput::A(None) => return Ok(0),
                         SelectOutput::B(_) => {
-                            let buf = self.write_buf.take().unwrap();
-                            let io = self.io.clone();
-                            self.write_task.set(async move {
-                                let (res, mut buf) = io.write(buf).await;
-                                (
-                                    res.map(|n| {
-                                        buf.advance(n);
-                                        n
-                                    }),
-                                    buf,
-                                )
+                            self.write_task.set_with(|buf| {
+                                let io = self.io.clone();
+                                async move {
+                                    let (res, mut buf) = io.write(buf).await;
+                                    (
+                                        res.map(|n| {
+                                            buf.advance(n);
+                                            n
+                                        }),
+                                        buf,
+                                    )
+                                }
                             });
                         }
                     }
@@ -159,17 +163,8 @@ where
             };
 
             match write_task.select(self.read_task.poll()).await {
-                SelectOutput::A((res, buf)) => {
-                    self.write_buf = Some(buf);
-                    let n = res?;
-                    if n == 0 {
-                        return Ok(None);
-                    }
-                }
-                SelectOutput::B((res, buf)) => {
-                    self.read_buf = Some(buf);
-                    let n = res?;
-                    if n == 0 {
+                SelectOutput::A(res) | SelectOutput::B(res) => {
+                    if res? == 0 {
                         return Ok(None);
                     }
                 }
