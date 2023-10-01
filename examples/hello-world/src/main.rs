7819faa9c5b9ecc5093d70773bc19697c4ec37b0
fakeshadow <24548779@qq.com>
2023-10-01 12:08:38 +0800

    update example. (#704)
    * update example.
    
    * install protoc in example check ci.

--- examples/hello-world/src/main.rs
+++ examples/hello-world/src/main.rs
@@ -3,12 +3,12 @@
 use xitca_web::{handler::handler_service, route::get, App, HttpServer};
 
 fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().at("/", get(handler_service(handler))).finish())
-        .bind("127.0.0.1:8080")?
-        .run()
-        .wait()
-}
-
-async fn handler() -> &'static str {
-    "Hello,World!"
+    HttpServer::new(|| {
+        App::new()
+            .at("/", get(handler_service(|| async { "Hello,World!" })))
+            .finish()
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .wait()
 }
