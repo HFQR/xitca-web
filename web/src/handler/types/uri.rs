58d6451bc8c820c33653f69fdb7346c50476b46c
fakeshadow <24548779@qq.com>
2023-11-12 12:49:30 +0800

    add more extractor for owned value.

--- web/src/handler/types/uri.rs
+++ web/src/handler/types/uri.rs
@@ -30,3 +30,27 @@ where
         Ok(UriRef(req.req().uri()))
     }
 }
+
+#[derive(Debug)]
+pub struct UriOwn(pub Uri);
+
+impl Deref for UriOwn {
+    type Target = Uri;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'a, 'r, C, B> FromRequest<'a, WebRequest<'r, C, B>> for UriOwn
+where
+    B: BodyStream,
+{
+    type Type<'b> = UriOwn;
+    type Error = ExtractError<B::Error>;
+
+    #[inline]
+    async fn from_request(req: &'a WebRequest<'r, C, B>) -> Result<Self, Self::Error> {
+        Ok(UriOwn(req.req().uri().clone()))
+    }
+}
