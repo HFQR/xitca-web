031b56bd9331820afbb041e974d90b90781ffde2
fakeshadow <24548779@qq.com>
2023-11-14 11:26:31 +0800

    add certain module level doc for xitca-web crate. (#757)

--- web/src/middleware/mod.rs
+++ web/src/middleware/mod.rs
@@ -1,3 +1,5 @@
+//! middleware types.
+
 #[cfg(any(feature = "compress-br", feature = "compress-gz", feature = "compress-de"))]
 pub mod compress;
 #[cfg(any(feature = "compress-br", feature = "compress-gz", feature = "compress-de"))]
