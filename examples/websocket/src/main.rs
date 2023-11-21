194f39a2e61624a0cdc39f0eaff778f372048105
fakeshadow <24548779@qq.com>
2023-11-21 10:51:07 +0800

    remove all advance configuration for ws example.

--- examples/websocket/src/main.rs
+++ examples/websocket/src/main.rs
@@ -1,8 +1,5 @@
 //! A Http/1 server echos back websocket text message.
 
-use std::time::Duration;
-
-use tracing::{error, info};
 use xitca_web::{
     handler::{
         handler_service,
@@ -13,9 +10,6 @@ use xitca_web::{
 };
 
 fn main() -> std::io::Result<()> {
-    tracing_subscriber::fmt()
-        .with_env_filter("xitca=info,websocket=info")
-        .init();
     App::new()
         .at("/", get(handler_service(handler)))
         .serve()
@@ -26,17 +20,7 @@ fn main() -> std::io::Result<()> {
 
 async fn handler(mut ws: WebSocket) -> WebSocket {
     // config websocket.
-    ws
-        // interval of ping message sent to client.
-        .set_ping_interval(Duration::from_secs(5))
-        // max consecutive unanswered pings until server close connection
-        .set_max_unanswered_ping(2)
-        .on_msg(on_msg)
-        // async function that called when error occurred
-        .on_err(|e| async move { error!("{e}") })
-        // async function that called when closing websocket connection.
-        .on_close(|| async { info!("WebSocket connection closing") });
-
+    ws.on_msg(on_msg);
     // return the instance after configuration.
     ws
 }
@@ -45,7 +29,7 @@ async fn handler(mut ws: WebSocket) -> WebSocket {
 async fn on_msg(msg: Message, ctx: &mut Context) {
     // ignore non text message.
     if let Message::Text(txt) = msg {
-        info!("Got text message: {txt}");
+        println!("Got text message: {txt}");
         // echo back text.
         ctx.text(format!("Echo: {txt}")).await.unwrap();
     }
