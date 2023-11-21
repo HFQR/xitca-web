7c7daef093cb0666872255e9318bdc5fb9d64a77
fakeshadow <24548779@qq.com>
2023-11-21 14:06:58 +0800

    simplify by removing advanced configuration.

--- examples/websocket/src/main.rs
+++ examples/websocket/src/main.rs
@@ -1,8 +1,5 @@
 //! A Http/1 server echos back websocket text message.
 
-use std::time::Duration;
-
-use tracing::{error, info};
 use xitca_web::{
     handler::{
         handler_service,
@@ -13,9 +10,6 @@ use xitca_web::{
 };
 
 fn main() -> std::io::Result<()> {
-    tracing_subscriber::fmt()
-        .with_env_filter("xitca=info,websocket=info")
-        .init();
     App::new()
         .at("/", get(handler_service(handler)))
         .serve()
@@ -26,27 +20,17 @@ fn main() -> std::io::Result<()> {
 
 async fn handler(mut ws: WebSocket) -> WebSocket {
     // config websocket.
-    ws
-        // interval of ping message sent to client.
-        .set_ping_interval(Duration::from_secs(5))
-        // max consecutive unanswered pings until server close connection
-        .set_max_unanswered_ping(2)
+    ws.on_msg(|tx, msg| {
         // async function that called on every incoming websocket message.
-        .on_msg(|tx, msg| {
-            Box::pin(async move {
-                // ignore non text message.
-                if let Message::Text(txt) = msg {
-                    info!("Got text message: {txt}");
-                    // echo back text.
-                    tx.text(format!("Echo: {txt}")).await.unwrap();
-                }
-            })
+        Box::pin(async move {
+            // ignore non text message.
+            if let Message::Text(txt) = msg {
+                println!("Got text message: {txt}");
+                // echo back text.
+                tx.text(format!("Echo: {txt}")).await.unwrap();
+            }
         })
-        // async function that called when error occurred
-        .on_err(|e| async move { error!("{e}") })
-        // async function that called when closing websocket connection.
-        .on_close(|| async { info!("WebSocket connection closing") });
-
+    });
     // return the instance after configuration.
     ws
 }
