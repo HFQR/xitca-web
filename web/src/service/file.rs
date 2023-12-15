962f08db33e2e3c4da542e9b61078f500fb91106
fakeshadow <24548779@qq.com>
2023-12-16 07:23:02 +0800

    use a different approach when register file service route. (#801)
    * use a different approach when register file service route.
    
    * inline 304 response handling instead of box it in error.

--- web/src/service/file.rs
+++ web/src/service/file.rs
@@ -3,17 +3,14 @@ use core::convert::Infallible;
 use std::{borrow::Cow, path::PathBuf};
 
 use http_file::{ServeDir as _ServeDir, ServeError};
-use xitca_http::{
-    http::StatusCode,
-    util::service::router::{RouterGen, RouterMapErr},
-};
+use xitca_http::{http::StatusCode, util::service::router::RouterGen};
 
 use crate::{
     body::ResponseBody,
     bytes::Bytes,
     context::WebContext,
     dev::service::Service,
-    error::{BadRequest, Error, Internal, MatchError, MethodNotAllowed},
+    error::{BadRequest, Error, Internal, MatchError, MethodNotAllowed, RouterError},
     http::{Method, WebResponse},
 };
 
@@ -30,7 +27,7 @@ impl ServeDir {
 }
 
 impl RouterGen for ServeDir {
-    type Route<R> = RouterMapErr<R>;
+    type Route<R> = R;
 
     fn path_gen(&mut self, prefix: &'static str) -> Cow<'static, str> {
         let mut path = String::from(prefix);
@@ -44,7 +41,7 @@ impl RouterGen for ServeDir {
     }
 
     fn route_gen<R>(route: R) -> Self::Route<R> {
-        RouterMapErr(route)
+        route
     }
 }
 
@@ -61,33 +58,24 @@ pub struct ServeDirService(_ServeDir);
 
 impl<'r, C, B> Service<WebContext<'r, C, B>> for ServeDirService {
     type Response = WebResponse;
-    type Error = Error<C>;
-
-    async fn call(&self, req: WebContext<'r, C, B>) -> Result<Self::Response, Self::Error> {
-        self.0
-            .serve(req.req())
-            .await
-            .map(|res| res.map(ResponseBody::box_stream))
-            .map_err(Error::from_service)
-    }
-}
-
-impl<'r, C, B> Service<WebContext<'r, C, B>> for ServeError {
-    type Response = WebResponse;
-    type Error = Infallible;
+    type Error = RouterError<Error<C>>;
 
     async fn call(&self, ctx: WebContext<'r, C, B>) -> Result<Self::Response, Self::Error> {
-        match self {
-            Self::NotFound => MatchError::NotFound.call(ctx).await,
-            Self::MethodNotAllowed => MethodNotAllowed(vec![Method::GET, Method::HEAD]).call(ctx).await,
-            Self::NotModified => {
+        match self.0.serve(ctx.req()).await {
+            Ok(res) => Ok(res.map(ResponseBody::box_stream)),
+            Err(ServeError::NotModified) => {
                 let mut res = ctx.into_response(Bytes::new());
                 *res.status_mut() = StatusCode::NOT_MODIFIED;
                 Ok(res)
             }
-            // TODO: better error handling.
-            Self::Io(_) => Internal.call(ctx).await,
-            _ => BadRequest.call(ctx).await,
+            Err(e) => Err(match e {
+                ServeError::NotFound => RouterError::Match(MatchError::NotFound),
+                ServeError::MethodNotAllowed => {
+                    RouterError::NotAllowed(MethodNotAllowed(vec![Method::GET, Method::HEAD]))
+                }
+                ServeError::Io(_) => RouterError::Service(Error::from_service(Internal)),
+                _ => RouterError::Service(Error::from_service(BadRequest)),
+            }),
         }
     }
 }
