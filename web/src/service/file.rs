1c5ab8bb46fbca4fdb6ec8308aab7257259ad6c0
fakeshadow <24548779@qq.com>
2023-12-15 22:35:54 +0800

    use a different approach when register file service route.

--- web/src/service/file.rs
+++ web/src/service/file.rs
@@ -1,19 +1,16 @@
-use core::convert::Infallible;
+use core::{convert::Infallible, fmt};
 
-use std::{borrow::Cow, path::PathBuf};
+use std::{borrow::Cow, error, path::PathBuf};
 
 use http_file::{ServeDir as _ServeDir, ServeError};
-use xitca_http::{
-    http::StatusCode,
-    util::service::router::{RouterGen, RouterMapErr},
-};
+use xitca_http::{http::StatusCode, util::service::router::RouterGen};
 
 use crate::{
     body::ResponseBody,
     bytes::Bytes,
     context::WebContext,
     dev::service::Service,
-    error::{BadRequest, Error, Internal, MatchError, MethodNotAllowed},
+    error::{BadRequest, Error, Internal, MatchError, MethodNotAllowed, RouterError},
     http::{Method, WebResponse},
 };
 
@@ -30,7 +27,7 @@ impl ServeDir {
 }
 
 impl RouterGen for ServeDir {
-    type Route<R> = RouterMapErr<R>;
+    type Route<R> = R;
 
     fn path_gen(&mut self, prefix: &'static str) -> Cow<'static, str> {
         let mut path = String::from(prefix);
@@ -44,7 +41,7 @@ impl RouterGen for ServeDir {
     }
 
     fn route_gen<R>(route: R) -> Self::Route<R> {
-        RouterMapErr(route)
+        route
     }
 }
 
@@ -61,33 +58,43 @@ pub struct ServeDirService(_ServeDir);
 
 impl<'r, C, B> Service<WebContext<'r, C, B>> for ServeDirService {
     type Response = WebResponse;
-    type Error = Error<C>;
+    type Error = RouterError<Error<C>>;
 
     async fn call(&self, req: WebContext<'r, C, B>) -> Result<Self::Response, Self::Error> {
         self.0
             .serve(req.req())
             .await
             .map(|res| res.map(ResponseBody::box_stream))
-            .map_err(Error::from_service)
+            .map_err(|e| match e {
+                ServeError::NotFound => RouterError::Match(MatchError::NotFound),
+                ServeError::MethodNotAllowed => {
+                    RouterError::NotAllowed(MethodNotAllowed(vec![Method::GET, Method::HEAD]))
+                }
+                ServeError::NotModified => RouterError::Service(Error::from_service(NotModified)),
+                ServeError::Io(_) => RouterError::Service(Error::from_service(Internal)),
+                _ => RouterError::Service(Error::from_service(BadRequest)),
+            })
     }
 }
 
-impl<'r, C, B> Service<WebContext<'r, C, B>> for ServeError {
+#[derive(Debug)]
+pub struct NotModified;
+
+impl fmt::Display for NotModified {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("File not Modified")
+    }
+}
+
+impl error::Error for NotModified {}
+
+impl<'r, C, B> Service<WebContext<'r, C, B>> for NotModified {
     type Response = WebResponse;
     type Error = Infallible;
 
     async fn call(&self, ctx: WebContext<'r, C, B>) -> Result<Self::Response, Self::Error> {
-        match self {
-            Self::NotFound => MatchError::NotFound.call(ctx).await,
-            Self::MethodNotAllowed => MethodNotAllowed(vec![Method::GET, Method::HEAD]).call(ctx).await,
-            Self::NotModified => {
-                let mut res = ctx.into_response(Bytes::new());
-                *res.status_mut() = StatusCode::NOT_MODIFIED;
-                Ok(res)
-            }
-            // TODO: better error handling.
-            Self::Io(_) => Internal.call(ctx).await,
-            _ => BadRequest.call(ctx).await,
-        }
+        let mut res = ctx.into_response(Bytes::new());
+        *res.status_mut() = StatusCode::NOT_MODIFIED;
+        Ok(res)
     }
 }
