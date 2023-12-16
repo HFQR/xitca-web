1161e02f4a95dd2606a94feba3d7f3e81f94cf13
fakeshadow <24548779@qq.com>
2023-12-16 16:39:07 +0800

    fix service impl for ServeDir (#804)
    * fix service impl for ServeDir
    
    * fix import.

--- examples/file/src/main.rs
+++ examples/file/src/main.rs
@@ -2,13 +2,17 @@
 //! example require nightly Rust to compile
 //! example assume you run the application from the path of /examples/file/
 
-use xitca_web::{middleware::compress::Compress, service::file::ServeDir, App};
+use xitca_web::{dev::service::ServiceExt, middleware::compress::Compress, service::file::ServeDir, App};
 
 fn main() -> std::io::Result<()> {
     println!("open http://localhost:8080/index.html in browser to visit the site");
     App::new()
-        .at("/", ServeDir::new("static"))
-        .enclosed(Compress)
+        /*
+            map "/" prefix uri to ""./static" file path. for example http request with uri of
+            "/index.html" will be matched against "./static/index.html" file. then enclose the
+            serve file service with compress middleware for file compression.
+        */
+        .at("/", ServeDir::new("static").enclosed(Compress))
         .serve()
         .bind("localhost:8080")?
         .run()
