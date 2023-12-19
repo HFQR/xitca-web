f35e59a381b0bbbf62e75f3087e72885d6a4f3a6
fakeshadow <24548779@qq.com>
2023-12-20 06:25:45 +0800

    move web error impl to error module. (#822)

--- web/src/error.rs
+++ web/src/error.rs
@@ -19,9 +19,8 @@ pub use xitca_http::{
 use crate::{
     bytes::Bytes,
     context::WebContext,
-    http::StatusCode,
-    http::WebResponse,
-    service::{object::ServiceObject, Service},
+    http::{header::ALLOW, StatusCode, WebResponse},
+    service::Service,
 };
 
 use self::service_impl::ErrorService;
@@ -131,7 +130,7 @@ impl<'r, C> Service<WebContext<'r, C>> for Error<C> {
     type Error = Infallible;
 
     async fn call(&self, ctx: WebContext<'r, C>) -> Result<Self::Response, Self::Error> {
-        ServiceObject::call(&***self, ctx).await
+        crate::service::object::ServiceObject::call(&***self, ctx).await
     }
 }
 
@@ -179,7 +178,72 @@ impl<'r, C, B> Service<WebContext<'r, C, B>> for BadRequest {
     }
 }
 
+macro_rules! blank_error_service {
+    ($type: ty, $status: path) => {
+        impl<'r, C, B> Service<WebContext<'r, C, B>> for $type {
+            type Response = WebResponse;
+            type Error = Infallible;
+
+            async fn call(&self, ctx: WebContext<'r, C, B>) -> Result<Self::Response, Self::Error> {
+                let mut res = ctx.into_response(Bytes::new());
+                *res.status_mut() = $status;
+                Ok(res)
+            }
+        }
+    };
+}
+
+blank_error_service!(MatchError, StatusCode::NOT_FOUND);
+blank_error_service!(std::io::Error, StatusCode::INTERNAL_SERVER_ERROR);
+blank_error_service!(Box<dyn error::Error>, StatusCode::INTERNAL_SERVER_ERROR);
+blank_error_service!(Box<dyn error::Error + Send>, StatusCode::INTERNAL_SERVER_ERROR);
+blank_error_service!(Box<dyn error::Error + Send + Sync>, StatusCode::INTERNAL_SERVER_ERROR);
+
+impl<'r, C, B, E> Service<WebContext<'r, C, B>> for RouterError<E>
+where
+    E: for<'r2> Service<WebContext<'r2, C, B>, Response = WebResponse, Error = Infallible>,
+{
+    type Response = WebResponse;
+    type Error = Infallible;
+
+    async fn call(&self, ctx: WebContext<'r, C, B>) -> Result<Self::Response, Self::Error> {
+        match *self {
+            Self::Match(ref e) => e.call(ctx).await,
+            Self::NotAllowed(ref e) => e.call(ctx).await,
+            Self::Service(ref e) => e.call(ctx).await,
+        }
+    }
+}
+
+impl<'r, C, B> Service<WebContext<'r, C, B>> for MethodNotAllowed {
+    type Response = WebResponse;
+    type Error = Infallible;
+
+    async fn call(&self, ctx: WebContext<'r, C, B>) -> Result<Self::Response, Self::Error> {
+        let mut res = ctx.into_response(Bytes::new());
+
+        let allowed = self.allowed_methods();
+
+        let len = allowed.iter().fold(0, |a, m| a + m.as_str().len() + 1);
+
+        let mut methods = String::with_capacity(len);
+
+        for method in allowed {
+            methods.push_str(method.as_str());
+            methods.push(',');
+        }
+        methods.pop();
+
+        res.headers_mut().insert(ALLOW, methods.parse().unwrap());
+        *res.status_mut() = StatusCode::METHOD_NOT_ALLOWED;
+
+        Ok(res)
+    }
+}
+
 mod service_impl {
+    use crate::service::object::ServiceObject;
+
     use super::*;
 
     pub trait ErrorService<Req>:
