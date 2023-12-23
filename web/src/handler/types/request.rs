582039c8683978e813480944435455e3640b5822
fakeshadow <24548779@qq.com>
2023-12-23 16:36:12 +0800

    add extractor for (&)WebRequest<()> and (&)RequestExt<()>

--- web/src/handler/types/request.rs
+++ web/src/handler/types/request.rs
@@ -1,35 +1,61 @@
-//! type extractor for [Request] and [RequestExt]
-
-use core::ops::Deref;
+//! type extractor for [WebRequest] and [RequestExt]
 
 use crate::{
     body::BodyStream,
     context::WebContext,
     error::Error,
     handler::FromRequest,
-    http::{Request, RequestExt},
+    http::{RequestExt, WebRequest},
 };
 
-#[derive(Debug)]
-pub struct RequestRef<'a>(pub &'a Request<RequestExt<()>>);
-
-impl Deref for RequestRef<'_> {
-    type Target = Request<RequestExt<()>>;
-
-    fn deref(&self) -> &Self::Target {
-        self.0
-    }
-}
-
-impl<'a, 'r, C, B> FromRequest<'a, WebContext<'r, C, B>> for RequestRef<'a>
+impl<'a, 'r, C, B> FromRequest<'a, WebContext<'r, C, B>> for &'a WebRequest<()>
 where
     B: BodyStream,
 {
-    type Type<'b> = RequestRef<'b>;
+    type Type<'b> = &'b WebRequest<()>;
     type Error = Error<C>;
 
     #[inline]
     async fn from_request(ctx: &'a WebContext<'r, C, B>) -> Result<Self, Self::Error> {
-        Ok(RequestRef(ctx.req()))
+        Ok(ctx.req())
+    }
+}
+
+impl<'a, 'r, C, B> FromRequest<'a, WebContext<'r, C, B>> for &'a RequestExt<()>
+where
+    B: BodyStream,
+{
+    type Type<'b> = &'b WebRequest<()>;
+    type Error = Error<C>;
+
+    #[inline]
+    async fn from_request(ctx: &'a WebContext<'r, C, B>) -> Result<Self, Self::Error> {
+        Ok(ctx.req().body())
+    }
+}
+
+impl<'a, 'r, C, B> FromRequest<'a, WebContext<'r, C, B>> for WebRequest<()>
+where
+    B: BodyStream,
+{
+    type Type<'b> = WebRequest<()>;
+    type Error = Error<C>;
+
+    #[inline]
+    async fn from_request(ctx: &'a WebContext<'r, C, B>) -> Result<Self, Self::Error> {
+        Ok(ctx.req().clone())
+    }
+}
+
+impl<'a, 'r, C, B> FromRequest<'a, WebContext<'r, C, B>> for RequestExt<()>
+where
+    B: BodyStream,
+{
+    type Type<'b> = &'b WebRequest<()>;
+    type Error = Error<C>;
+
+    #[inline]
+    async fn from_request(ctx: &'a WebContext<'r, C, B>) -> Result<Self, Self::Error> {
+        Ok(ctx.req().body().clone())
     }
 }
