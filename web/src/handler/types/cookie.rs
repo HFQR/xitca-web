9c32c446ac669745bfdd7a527236b9fce61bfc3e
fakeshadow <24548779@qq.com>
2023-12-24 10:32:04 +0800

    add secure cookie.

--- web/src/handler/types/cookie.rs
+++ web/src/handler/types/cookie.rs
@@ -1,6 +1,6 @@
-use core::{convert::Infallible, marker::PhantomData};
+use core::{convert::Infallible, marker::PhantomData, ops::Deref};
 
-pub use cookie::{Cookie, ParseError};
+pub use cookie::{Cookie, Key, ParseError};
 
 use cookie::CookieJar as _CookieJar;
 
@@ -17,12 +17,12 @@ use crate::{
     WebContext,
 };
 
-pub struct CookieJar<M = marker::Plain> {
+pub struct CookieJar<K = NoKey> {
     jar: _CookieJar,
-    _marker: PhantomData<M>,
+    key: K,
 }
 
-impl<M> CookieJar<M> {
+impl CookieJar<NoKey> {
     #[inline]
     pub fn get(&self, name: &str) -> Option<&Cookie> {
         self.jar.get(name)
@@ -45,18 +45,93 @@ impl<M> CookieJar<M> {
     }
 }
 
-mod marker {
-    pub struct Plain;
-}
+#[doc(hidden)]
+pub struct NoKey;
 
-impl<'a, 'r, C, B> FromRequest<'a, WebContext<'r, C, B>> for CookieJar<marker::Plain>
+impl<'a, 'r, C, B> FromRequest<'a, WebContext<'r, C, B>> for NoKey
 where
     B: BodyStream,
 {
-    type Type<'b> = CookieJar<marker::Plain>;
+    type Type<'b> = Self;
+    type Error = Error<C>;
+
+    #[inline]
+    async fn from_request(_: &'a WebContext<'r, C, B>) -> Result<Self, Self::Error> {
+        Ok(Self)
+    }
+}
+
+macro_rules! cookie_variant {
+    ($variant: tt, $method: tt, $method_mut: tt) => {
+        pub struct $variant<K> {
+            key: Key,
+            _key: PhantomData<fn(K)>,
+        }
+
+        impl<K> Deref for $variant<K> {
+            type Target = Key;
+
+            fn deref(&self) -> &Self::Target {
+                &self.key
+            }
+        }
+
+        impl<K> CookieJar<$variant<K>> {
+            #[inline]
+            pub fn get(&self, name: &str) -> Option<Cookie> {
+                self.jar.$method(&self.key).get(name)
+            }
+
+            #[inline]
+            pub fn add<C>(&mut self, cookie: C)
+            where
+                C: Into<Cookie<'static>>,
+            {
+                self.jar.$method_mut(&self.key).add(cookie)
+            }
+
+            #[inline]
+            pub fn remove<C>(&mut self, cookie: C)
+            where
+                C: Into<Cookie<'static>>,
+            {
+                self.jar.$method_mut(&self.key).remove(cookie)
+            }
+        }
+
+        impl<'a, 'r, C, B, K> FromRequest<'a, WebContext<'r, C, B>> for $variant<K>
+        where
+            K: for<'a2, 'r2> FromRequest<'a2, WebContext<'r2, C, B>, Error = Error<C>> + Into<Key>,
+            B: BodyStream,
+        {
+            type Type<'b> = Self;
+            type Error = Error<C>;
+
+            #[inline]
+            async fn from_request(ctx: &'a WebContext<'r, C, B>) -> Result<Self, Self::Error> {
+                K::from_request(ctx).await.map(|key| $variant {
+                    key: key.into(),
+                    _key: PhantomData,
+                })
+            }
+        }
+    };
+}
+
+cookie_variant!(Private, private, private_mut);
+cookie_variant!(Signed, signed, signed_mut);
+
+impl<'a, 'r, C, B, K> FromRequest<'a, WebContext<'r, C, B>> for CookieJar<K>
+where
+    K: for<'a2, 'r2> FromRequest<'a2, WebContext<'r2, C, B>, Error = Error<C>>,
+    B: BodyStream,
+{
+    type Type<'b> = CookieJar<K>;
     type Error = Error<C>;
 
     async fn from_request(ctx: &'a WebContext<'r, C, B>) -> Result<Self, Self::Error> {
+        let key = K::from_request(ctx).await?;
+
         let mut jar = _CookieJar::new();
 
         for val in ctx.req().headers().get_all(COOKIE).into_iter() {
@@ -66,10 +141,7 @@ where
             }
         }
 
-        Ok(CookieJar {
-            jar,
-            _marker: PhantomData,
-        })
+        Ok(CookieJar { jar, key })
     }
 }
 
@@ -79,7 +151,7 @@ forward_blank_bad_request!(ToStrError);
 error_from_service!(ParseError);
 forward_blank_bad_request!(ParseError);
 
-impl<'r, C, B> Responder<WebContext<'r, C, B>> for CookieJar<marker::Plain> {
+impl<'r, C, B> Responder<WebContext<'r, C, B>> for CookieJar<NoKey> {
     type Response = WebResponse;
     type Error = Error<C>;
 
@@ -113,7 +185,7 @@ mod test {
         let mut ctx = ctx.as_web_ctx();
         ctx.req_mut().headers_mut().insert("cookie", "foo=bar".parse().unwrap());
 
-        let mut jar = CookieJar::from_request(&ctx).now_or_panic().unwrap();
+        let mut jar: CookieJar = CookieJar::from_request(&ctx).now_or_panic().unwrap();
 
         let val = jar.get("foo").unwrap();
         assert_eq!(val.name(), "foo");
@@ -125,5 +197,66 @@ mod test {
 
         let header = res.headers().get(SET_COOKIE).unwrap();
         assert_eq!(header.to_str().unwrap(), "996=251");
+    }
+
+    #[derive(Clone)]
+    struct MyKey(Key);
+
+    impl From<MyKey> for Key {
+        fn from(value: MyKey) -> Self {
+            value.0
+        }
+    }
+
+    impl<'a, 'r, C, B> FromRequest<'a, WebContext<'r, C, B>> for MyKey {
+        type Type<'b> = MyKey;
+        type Error = Error<C>;
+
+        async fn from_request(ctx: &'a WebContext<'r, C, B>) -> Result<Self, Self::Error> {
+            Ok(ctx.req().extensions().get::<MyKey>().unwrap().clone())
+        }
+    }
+
+    #[test]
+    fn private_cookie() {
+        let mut ctx = WebContext::new_test(&());
+        let mut ctx = ctx.as_web_ctx();
+
+        let key = Key::generate();
+
+        let mut jar = _CookieJar::new();
+        jar.private_mut(&key).add(Cookie::new("foo", "bar"));
+        let cookie = jar.delta().next().unwrap().to_string().try_into().unwrap();
+
+        ctx.req_mut().headers_mut().insert(COOKIE, cookie);
+        ctx.req_mut().extensions_mut().insert(MyKey(key));
+
+        let jar: CookieJar<Private<MyKey>> = CookieJar::<Private<MyKey>>::from_request(&ctx).now_or_panic().unwrap();
+
+        let val = jar.get("foo").unwrap();
+        assert_eq!(val.name(), "foo");
+        assert_eq!(val.value(), "bar");
+    }
+
+    #[test]
+    fn signed_cookie() {
+        let mut ctx = WebContext::new_test(&());
+        let mut ctx = ctx.as_web_ctx();
+
+        let key = Key::generate();
+
+        let mut jar = _CookieJar::new();
+        jar.signed_mut(&key).add(Cookie::new("foo", "bar"));
+
+        let cookie = jar.delta().next().unwrap().to_string().try_into().unwrap();
+
+        ctx.req_mut().headers_mut().insert(COOKIE, cookie);
+        ctx.req_mut().extensions_mut().insert(MyKey(key));
+
+        let jar: CookieJar<Signed<MyKey>> = CookieJar::<Signed<MyKey>>::from_request(&ctx).now_or_panic().unwrap();
+
+        let val = jar.get("foo").unwrap();
+        assert_eq!(val.name(), "foo");
+        assert_eq!(val.value(), "bar");
     }
 }
