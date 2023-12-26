e9e48e60a95f08dea42fcc2613e6cd1e9cf3aa97
fakeshadow <24548779@qq.com>
2023-12-26 11:38:52 +0800

    add doc for lazy extractor.

--- web/src/handler/types/lazy.rs
+++ web/src/handler/types/lazy.rs
@@ -4,7 +4,40 @@ use core::marker::PhantomData;
 
 use std::borrow::Cow;
 
-// lazy deserialize type that wrap around other extractor type like `Json`, `Form` and `Query`
+/// lazy deserialize type that wrap around other extractor type like `Json`, `Form`, `Params` 
+/// and `Query`. It lowers the deserialization to handler function where zero copy deserialize 
+/// can happen.
+///
+/// # Example
+/// ```rust(no_run)
+/// use serde::Deserialize;
+/// use xitca_web::{
+///   error::Error,
+///   http::StatusCode,
+///   handler::{handler_service, json::Json, lazy::Lazy},
+///   App, WebContext
+/// };
+///
+/// // a json object with zero copy deserialization.
+/// #[derive(Deserialize)]
+/// struct Post<'a> {
+///     title: &'a str,
+///     content: &'a str    
+/// }
+///
+/// // handler function utilize Lazy type to lower the Json type into handler function.
+/// async fn handler(lazy: Lazy<'static, Json<Post<'_>>>) -> Result<String, Error> {
+///     // actual deserialize happens here.
+///     let Post { title, content } = lazy.deserialize()?;
+///     // the Post type and it's &str referencing lazy would live until the handler function
+///     // returns.
+///     Ok(format!("Post {{ title: {title}, content: {content} }}"))
+/// }
+///
+/// App::new()
+///     .at("/post", handler_service(handler))
+///     .at("/", handler_service(|_: &WebContext<'_>| async { "used for infer type" }));
+/// ```
 pub struct Lazy<'a, T> {
     inner: Cow<'a, [u8]>,
     _type: PhantomData<T>,
