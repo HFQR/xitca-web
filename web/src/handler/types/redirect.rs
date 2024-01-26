3cd099d18c7786e291b6c6dd475485bd4961d5c8
fakeshadow <24548779@qq.com>
2024-01-27 03:37:46 +0800

    add router service impl for Redirect and Html types. (#912)

--- web/src/handler/types/redirect.rs
+++ web/src/handler/types/redirect.rs
@@ -1,5 +1,9 @@
 //! type responder for http redirecting response.
 
+use core::convert::Infallible;
+
+use xitca_http::util::service::router::{RouterGen, RouterMapErr};
+
 use crate::{
     body::ResponseBody,
     error::{Error, Internal},
@@ -8,9 +12,11 @@ use crate::{
         header::{HeaderValue, LOCATION},
         StatusCode, WebResponse,
     },
+    service::Service,
     WebContext,
 };
 
+#[derive(Clone)]
 pub struct Redirect {
     status: StatusCode,
     location: Result<HeaderValue, Internal>,
@@ -55,23 +61,65 @@ impl<'r, C, B> Responder<WebContext<'r, C, B>> for Redirect {
     }
 }
 
+impl RouterGen for Redirect {
+    type Route<R> = RouterMapErr<R>;
+
+    fn route_gen<R>(route: R) -> Self::Route<R> {
+        RouterMapErr(route)
+    }
+}
+
+impl Service for Redirect {
+    type Response = Self;
+    type Error = Infallible;
+
+    async fn call(&self, _: ()) -> Result<Self::Response, Self::Error> {
+        Ok(self.clone())
+    }
+}
+
+impl<'r, C, B> Service<WebContext<'r, C, B>> for Redirect {
+    type Response = WebResponse;
+    type Error = Error<C>;
+
+    #[inline]
+    async fn call(&self, ctx: WebContext<'r, C, B>) -> Result<Self::Response, Self::Error> {
+        self.clone().respond(ctx).await
+    }
+}
+
 #[cfg(test)]
 mod test {
     use xitca_unsafe_collection::futures::NowOrPanic;
 
+    use crate::{http::WebRequest, App};
+
     use super::*;
 
     #[test]
-    fn redirect() {
+    fn respond() {
         let redirect = Redirect::see_other("/996");
 
         let mut ctx = WebContext::new_test(&());
         let ctx = ctx.as_web_ctx();
 
         let res = redirect.respond(ctx).now_or_panic().unwrap();
-
         assert_eq!(res.status().as_u16(), 303);
+        assert_eq!(res.headers().get(LOCATION).unwrap().to_str().unwrap(), "/996")
+    }
 
+    #[test]
+    fn service() {
+        let res = App::new()
+            .at("/", Redirect::see_other("/996"))
+            .finish()
+            .call(())
+            .now_or_panic()
+            .unwrap()
+            .call(WebRequest::default())
+            .now_or_panic()
+            .unwrap();
+        assert_eq!(res.status().as_u16(), 303);
         assert_eq!(res.headers().get(LOCATION).unwrap().to_str().unwrap(), "/996")
     }
 }
