16548091a783c806880af5baa80f924ab6299d18
fakeshadow <24548779@qq.com>
2024-02-17 01:23:37 +0800

    minor fix.

--- examples/error-handle/src/main.rs
+++ examples/error-handle/src/main.rs
@@ -2,7 +2,7 @@
 //! code must be compiled with nightly Rust.
 
 // nightly rust features are not required for error handling in xitca-web. but they can enhance
-// it quite a bit with certain features and they are purely opt-in.
+// the experience quite a bit with certain features and they are purely opt-in.
 //
 // trait_upcasting nightly feature enables casting xitca_web::error::Error type to &dyn std::error::Error.
 // error_generic_member_access and error_reporter nightly feature enables stack backtrace capture and display.
@@ -11,8 +11,8 @@
 use std::{backtrace::Backtrace, convert::Infallible, error, fmt};
 
 use xitca_web::{
-    error::{Error, ErrorStatus},
-    handler::handler_service,
+    error::{Error, MatchError},
+    handler::{handler_service, html::Html, Responder},
     http::{StatusCode, WebResponse},
     route::get,
     service::Service,
@@ -51,9 +51,10 @@ async fn typed() -> Result<&'static str, MyError> {
 // a custom error type. must implement following traits:
 // std::fmt::{Debug, Display} for formatting
 // std::error::Error for backtrace and type casting
-// xitca_web::dev::service::Service for http response generation.
+// From for converting from Self to xitca_web::error::Error type.
+// xitca_web::service::Service for lazily generating http response.
 struct MyError {
-    // our error type provide thread backtrace which can be passed to xitca_web::error::Error instance.
+    // thread backtrace which can be requested after type erasing through std::error::Error::provide API.
     backtrace: Backtrace,
 }
 
@@ -110,14 +111,14 @@ where
 
             // generate http response actively. from here it's OK to early return it in Result::Ok
             // variant as error_handler function's output
-            let _res = e.call(ctx).await.unwrap();
+            let _res = e.call(ctx.reborrow()).await?;
             // return Ok(_res);
 
             // above are error handling enabled by stable rust.
 
             // below are error handling enabled by nightly rust.
 
-            // utilize std::error::Error trait methods for backtrace and more advanced error info.
+            // utilize std::error module for backtrace and more advanced error info.
             let report = error::Report::new(&e).pretty(true).show_backtrace(true);
             // display error report
             println!("{report}");
@@ -131,14 +132,15 @@ where
 
             // type casting can also be used to handle xitca-web's "internal" error types for overriding
             // default error behavior.
-            // *. "internal" means these error types have a default error formatter and http response generator.
+            // *. "internal" means these error types have their default error formatter and http response generator.
             // *. "internal" error types are public types exported through `xitca_web::error` module. it's OK to
             // override them for custom formatting/http response generating.
-            if let Some(e) = (&*e as &dyn error::Error).downcast_ref::<ErrorStatus>() {
-                // handle error generated from status code with error reporter.
-                let report = error::Report::new(e).pretty(true).show_backtrace(true);
-                // display error report
-                println!("{report}");
+            if (&*e as &dyn error::Error).downcast_ref::<MatchError>().is_some() {
+                // MatchError is error type for request not matching any route from application service.
+                // in this case we override it's default behavior by generating a different http response.
+                return (Html("<h1>404 Not Found</h1>"), StatusCode::NOT_FOUND)
+                    .respond(ctx)
+                    .await;
             }
 
             // the most basic error handling is to ignore it and return as is. xitca-web is able to take care
@@ -148,4 +150,5 @@ where
     }
 }
 
-// if you prefer proc macro please reference examples/macros
+// if you prefer proc macro please reference examples/macros where the trait implements can be drastically easier
+// at the cost of some compile time.
