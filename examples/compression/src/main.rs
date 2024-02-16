8ad737d01c8de9ece61b96fa1c7a9289fa64ae87
fakeshadow <24548779@qq.com>
2024-02-16 18:46:05 +0800

    update examples.

--- examples/compression/src/main.rs
+++ examples/compression/src/main.rs
@@ -5,18 +5,16 @@ use std::io;
 use tracing::info;
 use xitca_web::{
     handler::handler_service,
-    middleware::{compress::Compress, decompress::Decompress},
+    middleware::{compress::Compress, decompress::Decompress, Logger},
     App,
 };
 
 fn main() -> io::Result<()> {
-    tracing_subscriber::fmt()
-        .with_env_filter("xitca=trace,[xitca-logger]=trace")
-        .init();
     App::new()
         .at("/", handler_service(root))
         .enclosed(Compress)
         .enclosed(Decompress)
+        .enclosed(Logger::new())
         .serve()
         .bind("127.0.0.1:8080")?
         .run()
--- examples/file/src/main.rs
+++ examples/file/src/main.rs
@@ -1,20 +1,33 @@
-//! static file serving
-//! example require nightly Rust to compile
+//! static file serving example.
+//! require nightly Rust to compile
 //! example assume you run the application from the path of /examples/file/
 
-use xitca_web::{middleware::compress::Compress, service::file::ServeDir, service::ServiceExt, App};
+use xitca_web::{
+    handler::redirect::Redirect,
+    middleware::{compress::Compress, Logger},
+    service::file::ServeDir,
+    App,
+};
 
 fn main() -> std::io::Result<()> {
-    println!("open http://localhost:8080/index.html in browser to visit the site");
-    App::new()
+    let app = App::new()
+        // redirect user visiting "/" path to index file.
+        // this service has higher priority than following serve dir service.
+        .at("/", Redirect::see_other("/index.html"))
         /*
-            map "/" prefix uri to "./static" file path. for example http request with uri of
-            "/index.html" will be matched against "./static/index.html" file. then enclose the
-            serve file service with compress middleware for file compression.
+            map "/" prefixed uri to "./static" file path. for example http request with uri of
+            "/index.html" will be matched against "./static/index.html" file.
         */
-        .at("/", ServeDir::new("static").enclosed(Compress))
+        .at("/", ServeDir::new("static"))
+        // compression middleware
+        .enclosed(Compress)
+        // logger middleware
+        .enclosed(Logger::new())
         .serve()
         .bind("localhost:8080")?
-        .run()
-        .wait()
+        .run();
+
+    tracing::info!("open http://localhost:8080/ in browser to visit the site");
+
+    app.wait()
 }
