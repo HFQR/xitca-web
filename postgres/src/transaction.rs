1f1545a911be229879adf5ce1ebf3612486d7e0f
fakeshadow <24548779@qq.com>
2024-09-09 20:10:27 +0800

    add transaction save point

--- postgres/src/transaction.rs
+++ postgres/src/transaction.rs
@@ -21,11 +21,56 @@ where
     C: TransactionOps,
 {
     client: &'a mut C,
+    save_point: SavePoint,
     state: State,
 }
 
+enum SavePoint {
+    None,
+    Auto { depth: u32 },
+    Custom { name: String, depth: u32 },
+}
+
+impl SavePoint {
+    fn nest_save_point(&self, name: Option<String>) -> Self {
+        match *self {
+            Self::None => match name {
+                Some(name) => SavePoint::Custom { name, depth: 1 },
+                None => SavePoint::Auto { depth: 1 },
+            },
+            Self::Auto { depth } | Self::Custom { depth, .. } => match name {
+                Some(name) => SavePoint::Custom { name, depth },
+                None => SavePoint::Auto { depth: depth + 1 },
+            },
+        }
+    }
+
+    fn save_point_query(&self) -> String {
+        match self {
+            Self::None => "SAVEPOINT".to_string(),
+            Self::Auto { depth } => format!("SAVEPOINT sp_{depth}"),
+            Self::Custom { name, .. } => format!("SAVEPOINT {name}"),
+        }
+    }
+
+    fn commit_query(&self) -> String {
+        match self {
+            Self::None => "COMMIT".to_string(),
+            Self::Auto { depth } => format!("RELEASE sp_{depth}"),
+            Self::Custom { name, .. } => format!("RELEASE {name}"),
+        }
+    }
+
+    fn rollback_query(&self) -> String {
+        match self {
+            Self::None => "ROLLBACK".to_string(),
+            Self::Auto { depth } => format!("ROLLBACK TO sp_{depth}"),
+            Self::Custom { name, .. } => format!("ROLLBACK TO {name}"),
+        }
+    }
+}
+
 enum State {
-    Begin,
     WantRollback,
     Finish,
 }
@@ -37,7 +82,7 @@ where
     fn drop(&mut self) {
         match self.state {
             State::WantRollback => self.do_rollback(),
-            State::Begin | State::Finish => {}
+            State::Finish => {}
         }
     }
 }
@@ -47,21 +92,21 @@ where
     C: TransactionOps,
 {
     pub(crate) async fn new(client: &mut C) -> Result<Transaction<'_, C>, Error> {
-        let mut tx = Transaction {
+        client._execute_simple("BEGIN").await?;
+        Ok(Transaction {
             client,
-            state: State::Begin,
-        };
-        tx.begin().await?;
-        Ok(tx)
+            save_point: SavePoint::None,
+            state: State::WantRollback,
+        })
     }
 
-    /// [Client::query] for transaction.
+    /// [`Client::query``] for transaction.
     #[inline]
     pub fn query<'a>(&mut self, stmt: &'a Statement, params: &[&(dyn ToSql + Sync)]) -> Result<RowStream<'a>, Error> {
         self.query_raw(stmt, slice_iter(params))
     }
 
-    /// [Client::query_raw] for transaction.
+    /// [`Client::query_raw``] for transaction.
     #[inline]
     pub fn query_raw<'a, I>(&mut self, stmt: &'a Statement, params: I) -> Result<RowStream<'a>, Error>
     where
@@ -70,29 +115,53 @@ where
         self.client._query_raw(stmt, params)
     }
 
+    /// Like [`Client::transaction`], but creates a nested transaction via a savepoint.
+    pub async fn transaction(&mut self) -> Result<Transaction<'_, C>, Error> {
+        self._save_point(None).await
+    }
+
+    /// Like [`Client::transaction`], but creates a nested transaction via a savepoint with the specified name.
+    pub async fn save_point<I>(&mut self, name: I) -> Result<Transaction<'_, C>, Error>
+    where
+        I: Into<String>,
+    {
+        self._save_point(Some(name.into())).await
+    }
+
+    /// Consumes the transaction, committing all changes made within it.
     pub async fn commit(mut self) -> Result<(), Error> {
+        let query = self.save_point.commit_query();
         self.client
-            ._execute_simple("COMMIT")
+            ._execute_simple(&query)
             .await
             .map(|_| self.state = State::Finish)
     }
 
+    /// Rolls the transaction back, discarding all changes made within it.
+    ///
+    /// This is equivalent to `Transaction`'s `Drop` implementation, but provides any error encountered to the caller.
     pub async fn rollback(mut self) -> Result<(), Error> {
+        let query = self.save_point.rollback_query();
         self.client
-            ._execute_simple("ROLLBACK")
+            ._execute_simple(&query)
             .await
             .map(|_| self.state = State::Finish)
     }
 
-    async fn begin(&mut self) -> Result<(), Error> {
-        self.client
-            ._execute_simple("BEGIN")
-            .await
-            .map(|_| self.state = State::WantRollback)
+    async fn _save_point(&mut self, name: Option<String>) -> Result<Transaction<'_, C>, Error> {
+        let save_point = self.save_point.nest_save_point(name);
+        self.client._execute_simple(&save_point.save_point_query()).await?;
+
+        Ok(Transaction {
+            client: self.client,
+            save_point,
+            state: State::WantRollback,
+        })
     }
 
     fn do_rollback(&mut self) {
-        drop(self.client._execute_simple("ROLLBACK"));
+        let query = self.save_point.rollback_query();
+        drop(self.client._execute_simple(&query));
     }
 }
 
