a7861c42775909599bdd4c082a66753b7ccc1a6d
fakeshadow <24548779@qq.com>
2026-02-25 21:13:28 +0800

    simplify null check

--- postgres/src/protocol.rs
+++ postgres/src/protocol.rs
@@ -2,7 +2,7 @@
 
 use xitca_io::bytes::{BufMut, BytesMut};
 
-use std::io;
+use std::{convert::Infallible, io};
 
 use super::error::Error;
 
@@ -26,21 +26,16 @@ where
     K: IntoIterator,
     K::IntoIter: ExactSizeIterator<Item = i16>,
 {
-    // portal and statement names are constructed inside library boundary and they dont contain c style null bytes.
-    // debug assertions is enough for catching violation.
-    #[cfg(debug_assertions)]
-    validate_cstr([portal_name, stmt_name]);
-
     buf.put_u8(b'B');
 
     write_body(buf, |buf| {
-        write_cstr(portal_name.as_bytes(), buf);
-        write_cstr(stmt_name.as_bytes(), buf);
+        write_cstr(portal_name, buf);
+        write_cstr(stmt_name, buf);
         write_counted(
             formats,
             |f, buf| {
                 buf.put_i16(f);
-                Ok(())
+                Ok::<_, Infallible>(())
             },
             buf,
         )?;
@@ -49,38 +44,36 @@ where
             result_formats.into_iter(),
             |f, buf| {
                 buf.put_i16(f);
-                Ok(())
+                Ok::<_, Infallible>(())
             },
             buf,
-        )?;
-
-        Ok(())
+        )
     })
 }
 
 #[inline]
-fn write_body<F, E>(buf: &mut BytesMut, f: F) -> Result<(), E>
+fn write_body<F>(buf: &mut BytesMut, f: F) -> Result<(), Error>
 where
-    F: FnOnce(&mut BytesMut) -> Result<(), E>,
-    E: From<io::Error>,
+    F: FnOnce(&mut BytesMut) -> Result<(), Error>,
 {
     let base = buf.len();
     buf.put_i32(0);
 
     f(buf)?;
 
-    let size = i32::from_usize(buf.len() - base)?;
+    let size = FromUsize::from_usize(buf.len() - base)?;
     buf.put_i32_at(base, size);
     Ok(())
 }
 
 #[inline]
-fn write_counted<I, T, F>(items: I, mut serializer: F, buf: &mut BytesMut) -> Result<(), Error>
+fn write_counted<I, T, F, E>(items: I, mut serializer: F, buf: &mut BytesMut) -> Result<(), Error>
 where
     I: ExactSizeIterator<Item = T>,
-    F: FnMut(T, &mut BytesMut) -> Result<(), Error>,
+    F: FnMut(T, &mut BytesMut) -> Result<(), E>,
+    Error: From<E>,
 {
-    let count = u16::from_usize(items.len())?;
+    let count = FromUsize::from_usize(items.len())?;
     buf.put_u16(count);
 
     for item in items {
@@ -90,16 +83,15 @@ where
     Ok(())
 }
 
-fn write_nullable<F, E>(serializer: F, buf: &mut BytesMut) -> Result<(), E>
+fn write_nullable<F>(serializer: F, buf: &mut BytesMut) -> Result<(), Error>
 where
-    F: FnOnce(&mut BytesMut) -> Result<IsNull, E>,
-    E: From<io::Error>,
+    F: FnOnce(&mut BytesMut) -> Result<IsNull, Error>,
 {
     let base = buf.len();
     buf.put_i32(0);
 
     let size = match serializer(buf)? {
-        IsNull::No => i32::from_usize(buf.len() - base - 4)?,
+        IsNull::No => FromUsize::from_usize(buf.len() - base - 4)?,
         IsNull::Yes => -1,
     };
     buf.put_i32_at(base, size);
@@ -108,22 +100,16 @@ where
 }
 
 #[inline]
-fn write_cstr(s: &[u8], buf: &mut BytesMut) {
-    buf.put_slice(s);
-    buf.put_u8(0);
-}
-
-#[cfg(debug_assertions)]
-fn validate_cstr<'s, S>(s: S)
-where
-    S: IntoIterator,
-    S::IntoIter: Iterator<Item = &'s str>,
-{
-    for s in s {
-        if s.as_bytes().contains(&0) {
-            panic!("input string: {s} contains embedded null")
-        }
+fn write_cstr(s: &str, buf: &mut BytesMut) {
+    // strings used inside library dont contain c style null bytes.
+    // debug assertions is enough for catching violation.
+    #[cfg(debug_assertions)]
+    if s.as_bytes().contains(&0) {
+        panic!("input string: {s} contains embedded null")
     }
+
+    buf.put_slice(s.as_bytes());
+    buf.put_u8(0);
 }
 
 trait FromUsize: Sized {
