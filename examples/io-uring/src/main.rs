37f38bee68a0f982b5f460e3dafa6824397a29fb
fakeshadow <24548779@qq.com>
2026-04-09 22:26:49 +0800

    update io-uring example

--- examples/io-uring/src/main.rs
+++ examples/io-uring/src/main.rs
@@ -1,54 +1,13 @@
-//! A Http/1 tls server returns Hello World String as Response.
-//!
-//! *. io_uring is a linux OS feature.
-//! *. random self signed cert is used for tls certification.
+//! A Http server returns Hello World String as Response with io-uring runtime
 
-use std::{convert::Infallible, io, sync::Arc};
+use xitca_web::{App, handler::handler_service, route::get};
 
-use rustls::ServerConfig;
-use xitca_http::{
-    HttpServiceBuilder, ResponseBody, h1,
-    http::{Request, RequestExt, Response, const_header_value::TEXT_UTF8, header::CONTENT_TYPE},
-};
-use xitca_service::{ServiceExt, fn_service};
-
-fn main() -> io::Result<()> {
-    xitca_server::Builder::new()
-        .bind(
-            "http/1",
-            "127.0.0.1:8080",
-            fn_service(handler).enclosed(
-                HttpServiceBuilder::h1()
-                    .io_uring() // specify io_uring flavor of http service.
-                    .rustls(tls_config()), // specify io_uring flavor of tls.
-            ),
-        )?
-        .build()
+fn main() -> std::io::Result<()> {
+    App::new()
+        .at("/", get(handler_service(async || "Hello,World!")))
+        .serve()
+        .enable_io_uring()
+        .bind("127.0.0.1:8080")?
+        .run()
         .wait()
-}
-
-async fn handler(_: Request<RequestExt<h1::RequestBody>>) -> Result<Response<ResponseBody>, Infallible> {
-    Ok(Response::builder()
-        .header(CONTENT_TYPE, TEXT_UTF8)
-        .body("Hello World from io_uring!".into())
-        .unwrap())
-}
-
-// rustls configuration.
-fn tls_config() -> Arc<ServerConfig> {
-    let subject_alt_names = vec!["127.0.0.1".to_string(), "localhost".to_string()];
-
-    let cert = rcgen::generate_simple_self_signed(subject_alt_names).unwrap();
-
-    let mut config = ServerConfig::builder()
-        .with_no_client_auth()
-        .with_single_cert(
-            vec![cert.cert.into()],
-            cert.signing_key.serialize_der().try_into().unwrap(),
-        )
-        .unwrap();
-
-    config.alpn_protocols = vec![b"http/1.1".to_vec()];
-
-    Arc::new(config)
 }
