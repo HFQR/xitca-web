7fa07dae7c6e678f7aa3b2b97c0b1e86ecfcf4a5
fakeshadow <24548779@qq.com>
2026-05-03 22:46:00 +0800

    fix prefix check for h2c (#1377)

--- http/src/h2/proto.rs
+++ http/src/h2/proto.rs
@@ -1,11 +1,12 @@
+mod last_stream_id;
+mod reset_counter;
+mod size;
+mod stream;
+mod threshold;
+
 pub(super) mod error;
 pub(super) mod flow;
 pub(super) mod frame;
 pub(super) mod hpack;
-pub(super) mod last_stream_id;
 pub(super) mod ping_pong;
-pub(super) mod reset_counter;
-pub(super) mod size;
-pub(super) mod stream;
-pub(super) mod threshold;
 pub(super) mod window;
